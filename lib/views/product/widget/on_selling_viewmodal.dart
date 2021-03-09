1a429fd5db6e7242b131e62ee6ab7fb1cba16489
Richard Muragijimana <beastar457@gmail.com>
2021-03-09 10:52:45 +0200

    selling a product

--- lib/views/product/widget/on_selling_viewmodal.dart
+++ lib/views/product/widget/on_selling_viewmodal.dart
@@ -104,88 +104,39 @@ class OnProductSellingViewModal extends BaseModel {
     });
   }
 
-  void saveOrder() {
-    // get item from order variable
-    // get the stock and save new stock
+  void saveOrder({double customAmount}) {
     final q = Query(_databaseService.db,
         'SELECT id,value,branchId,variantId,isActive,canTrackingStock,productId,lowStock,currentStock,supplyPrice,retailPrice,showLowStockAlert,channels,table WHERE table=\$T AND variantId=\$VID');
 
     q.parameters = {'T': AppTables.stock, 'VID': _variantStock[0].id};
     final stocks = q.execute();
+
     if (stocks.isNotEmpty) {
-      //get the sock and update it
-      final Document stockDocument =
-          _databaseService.getById(id: Stock.fromMap(stocks[0]).id);
-      final double value = stockDocument.properties['value'].asDouble;
-      stockDocument.properties['currentStock'].asDouble;
-      // FIXME: use currentStock to check quantity of item remain in stock instead of value
-      // update other code when adding stock on variant to reflect the changes
-      stockDocument.properties['value'] = value - _quantity;
-      _databaseService.update(document: stockDocument);
-      //create a new order and perist it to the database....
-      //first check if we have active order
-      final q = Query(_databaseService.db,
-          'SELECT id,table,branchId,reference,isDraft,orderType,subTotal,taxAmount,cashReceived,salesTotal,orderNote,status,variantId,productName,channels WHERE table=\$T AND active=\$A');
+      if (customAmount.abs() != 0) {
+        final Order order =
+            ProxyService.keypad.pendingOrder(customAmount: customAmount);
 
-      q.parameters = {'T': AppTables.order, 'A': true};
-      final order = q.execute();
-      if (order.isEmpty) {
-        final id = Uuid().v1();
-        final Document ord = _databaseService.insert(id: id, data: {
-          'curreny': 'RWF',
-          'orderNumber': Uuid().v1().substring(0, 4),
-          'reference': Uuid().v1().substring(0, 4),
-          'branchId': sharedStateService.branch.id,
-          'orderItems': [],
-          'isDraft': true,
-          'active': true,
-          'orderType': 'sales',
-          'cashReceived': 0.0,
-          'customerChangeDue': 0.0,
-          'table': AppTables.order,
+        final id5 = Uuid().v1();
+        _databaseService.insert(id: id5, data: {
+          'orderId': order.id,
           'variantId': _variantStock[0].id,
-          'price': _variantStock[0].retailPrice,
-          'taxAmount':
-              0.0, //FIXME: fix to the right amount of tax given to setting
+          'variantName': _variantStock[0].name,
+          'Note': 'Custom Amount',
+          'updatedAt': DateTime.now().toIso8601String(),
+          'createdAt': DateTime.now().toIso8601String(),
           'stockId': Stock.fromMap(stocks[0]).id,
-          'channels': [sharedStateService.user.id.toString()],
-          'subTotal': _amountTotal
+          'channels': [ProxyService.sharedState.user.id.toString()],
+          'reason': 'SOLD',
+          'table': AppTables.stockHistories,
+          'quantity': _quantity,
+          'cashReceived': customAmount,
+          'cashCollected': customAmount,
+          'id': id5,
+          'customerChangeDue': 0.0
         });
-        saveOrderDetail(
-            orderId: ord.ID,
-            quantity: _quantity,
-            stockId: Stock.fromMap(stocks[0]).id);
-      } else {
-        final Order ord = Order.fromMap(order[0]);
-        saveOrderDetail(
-            orderId: ord.id,
-            quantity: _quantity,
-            stockId: Stock.fromMap(stocks[0]).id);
       }
+      ProxyService.keypad.updateStock(
+          stockId: Stock.fromMap(stocks[0]).id, quantity: _quantity);
     }
-  }
-
-  void saveOrderDetail({String orderId, double quantity, String stockId}) {
-    final id = Uuid().v1();
-    _databaseService.insert(id: id, data: {
-      'id': id,
-      'productName': _variantStock[0].productName,
-      'variantName': _variantStock[0].name,
-      'canTrackStock': false,
-      'stockId': stockId,
-      'unit': _variantStock[0].unit,
-      'sku': _variantStock[0].sku,
-      'quantity': _quantity,
-      'orderId': orderId,
-      'taxRate': 18.0, //TODO: make this dynamic should comes from settings
-      'table': AppTables.orderDetail,
-      'variantId': _variantStock[0].id,
-      'price': _variantStock[0].retailPrice,
-      'taxAmount': 0.0, //FIXME: fix to the right amount of tax given to setting
-      'channels': [sharedStateService.user.id.toString()],
-      'subTotal': _amountTotal,
-      'createdAt': DateTime.now().toIso8601String(),
-      'updatedAt': DateTime.now().toIso8601String()
-    });
   }
 }
