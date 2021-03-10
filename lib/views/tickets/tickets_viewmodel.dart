0792c1579461d4c3b0071b7f122de1fc07ea8451
Richard Muragijimana <beastar457@gmail.com>
2021-03-10 09:50:07 +0200

    code cleanup
    Former-commit-id: 4cd98b15971e6ec9ddf2f50a02a943df4ae8b288

--- lib/views/tickets/tickets_viewmodel.dart
+++ lib/views/tickets/tickets_viewmodel.dart
@@ -17,30 +17,15 @@ class TicketsViewModel extends ReactiveViewModel {
   List<ReactiveServiceMixin> get reactiveServices => [ProxyService.sharedState];
 
   void loadTickets() {
-    // final ticketsQuery = Query(_databaseService.db,
-    //     'SELECT stocks_histories.id,stocks_histories.orderId,stocks_histories.createdAt,stocks_histories.reason,stocks_histories.channels,orders.orderNote FROM stocks_histories JOIN orders ON stocks_histories.orderId=orders.id WHERE orders.status=\$STATUS AND orders.active=\$ACTIVE');
-
-    // // final Order pOrder = ProxyService.keypad.pendingOrder(customAmount: 0.0);
-
-    // ticketsQuery.parameters = {
-    //   'STATUS': 'parked',
-    //   'ACTIVE': false,
-    // }; //looking for active order joined with stock_histories
-
     final ticketsQuery = Query(_databaseService.db,
         'SELECT id,orderId,ticketName,table,createdAt WHERE table=\$T');
-
-    // final Order pOrder = ProxyService.keypad.pendingOrder(customAmount: 0.0);
-
     ticketsQuery.parameters = {
       'T': AppTables.tickets,
     }; //looking for active order joined with stock_histories
-
     ticketsQuery.addChangeListener((List orders) {
       // String orderId;
       if (orders.isNotEmpty) {
         for (Map map in orders) {
-          // _databaseService.delete(id: map['id']);
           if (!_tickets.contains(Ticket.fromMap(map))) {
             _tickets.add(Ticket.fromMap(map));
           }
@@ -95,7 +80,6 @@ class TicketsViewModel extends ReactiveViewModel {
   }
 
   Future<void> saveNewTicket() async {
-    // print(ProxyService.sharedState.user.id.toString());
     if (_ticketName != null) {
       final Order pOrder = ProxyService.keypad.pendingOrder(customAmount: 0.0);
       //create a new ticket for this order
@@ -117,9 +101,7 @@ class TicketsViewModel extends ReactiveViewModel {
         pendingTicket.properties['orders'] = ods;
         ProxyService.database.update(document: pendingTicket);
       }
-      //create a ticke with a name then edit a ticket with orderId(s) added as array.
       final Document pending = ProxyService.database.getById(id: pOrder.id);
-
       pending.properties['active'] = false;
       pending.properties['draft'] = false;
       pending.properties['orderNote'] = 'parked';
