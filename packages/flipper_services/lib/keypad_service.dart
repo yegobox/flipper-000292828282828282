e3fb5dd11cc6f662d707a8b8cc449fadca99ec10
Richard Muragijimana <beastar457@gmail.com>
2021-03-20 13:45:28 +0200

    fixing s9 samsung glaxy issue
    Former-commit-id: c49fa544eeffb017be491eada40307d12c0e7374

--- packages/flipper_services/lib/keypad_service.dart
+++ packages/flipper_services/lib/keypad_service.dart
@@ -16,21 +16,26 @@ import 'package:uuid/uuid.dart';
 
 class KeyPadService with ReactiveServiceMixin {
   final RxValue<double> _totalAmount = RxValue<double>();
-  final _sharedStateService = locator<SharedStateService>();
+  final _state = locator<SharedStateService>();
   final RxValue<Order> order = RxValue<Order>(initial: null);
   Order get currentSales => order.value;
 
   final Logger log = Logging.getLogger('O2:)');
   double get totalAmount => _totalAmount.value;
-  final DatabaseService _databaseService = ProxyService.database;
+  final DatabaseService _db = ProxyService.database;
 
   /// create new order,this method assume a cashier is still in progress of taking order
   /// then the amount passed assume that we are dealing with custom item
   /// if the takeNewOrder is not passed it will keep updating the current active and draft order
   void sellCustomAmount(
       {double customAmount = 0.0, bool takeNewOrder = false}) {
-    final Document variation = _databaseService.getCustomProductVariant();
-    final String stockId = _databaseService.getStockIdGivenProductId(
+    _db.createCustomItem(
+        branchId: _state.branch.id,
+        userId: _state.user.id,
+        businessId: _state.business.id);
+    final Document variation = _db.getCustomProductVariant();
+
+    final String stockId = _db.getStockIdGivenProductId(
         variantId: Variation.fromMap(variation.jsonProperties).id);
 
     pendingOrder(
@@ -39,9 +44,9 @@ class KeyPadService with ReactiveServiceMixin {
         variation: Variation.fromMap(variation.jsonProperties));
     order.listen((order) {
       if (order != null && order.active && order.draft && !takeNewOrder) {
-        final Document orderDocument = _databaseService.getById(id: order.id);
+        final Document orderDocument = _db.getById(id: order.id);
         orderDocument.properties['cashReceived'] = customAmount;
-        _databaseService.update(document: orderDocument);
+        _db.update(document: orderDocument);
         _totalAmount.value = customAmount;
         notifyListeners();
         return;
@@ -54,14 +59,14 @@ class KeyPadService with ReactiveServiceMixin {
   /// Also it is very important to treat order as an item added to the
   void pendingOrder(
       {double customAmount, String stockId, Variation variation}) {
-    final q = Query(_databaseService.db,
+    final q = Query(_db.db,
         'SELECT  id , branchId ,variantId,stockId,variantName, reference, draft ,active , orderType , orderNUmber , subTotal , double , taxAmount , cashReceived , saleTotal  , orderNote  , status  , variationId  , productName , channels , customerChangeDue WHERE table=\$T AND draft=\$DRAFT');
     q.parameters = {'T': AppTables.order, 'DRAFT': true};
 
     q.addChangeListener((List results) {
       if (results.isEmpty) {
         final id4 = Uuid().v1();
-        final Document ordr = _databaseService.insert(id: id4, data: {
+        final Document ordr = _db.insert(id: id4, data: {
           'reference': id4.substring(0, 4),
           'orderNUmber': id4.substring(0, 5),
           'status': 'pending',
@@ -71,14 +76,14 @@ class KeyPadService with ReactiveServiceMixin {
           'active':
               true, //used to check if order is parked becomes parked when false.
           'draft': true,
-          'channels': [_sharedStateService.user.id.toString()],
+          'channels': [_state.user.id.toString()],
           'subTotal': customAmount,
           'table': AppTables.order,
           'cashReceived': customAmount,
           'customerChangeDue': 0.0,
           'id': id4,
           'stockId': stockId,
-          'branchId': _sharedStateService.branch.id,
+          'branchId': _state.branch.id,
           'createdAt': DateTime.now().toIso8601String(),
         });
         order.value = Order.fromMap(ordr.jsonProperties);
@@ -100,18 +105,17 @@ class KeyPadService with ReactiveServiceMixin {
 
   void cleanKeypad() {
     ProxyService.sharedState.clear.listen((e) {
-      if(ProxyService.database.db!=null){
-        
-      final q = Query(ProxyService.database.db,
-          'SELECT  id  WHERE table=\$T AND status=\$S');
-      q.parameters = {'T': AppTables.order, 'S': 'pending'};
-      final results = q.execute();
-      if (results.isNotEmpty) {
-        for (Map id in results) {
-          ProxyService.database.delete(id: id['id']);
+      if (ProxyService.database.db != null) {
+        final q = Query(ProxyService.database.db,
+            'SELECT  id  WHERE table=\$T AND status=\$S');
+        q.parameters = {'T': AppTables.order, 'S': 'pending'};
+        final results = q.execute();
+        if (results.isNotEmpty) {
+          for (Map id in results) {
+            ProxyService.database.delete(id: id['id']);
+          }
         }
-      }
-      notifyListeners();
+        notifyListeners();
       }
     });
   }
