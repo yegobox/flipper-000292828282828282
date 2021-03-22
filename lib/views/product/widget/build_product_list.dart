25b580fd63d63a53d8f690efaf8aee1625027d81
Richard Muragijimana <beastar457@gmail.com>
2021-03-22 11:56:48 +0200

    now about to work on charge button, can add regular item to current sale

--- lib/views/product/widget/build_product_list.dart
+++ lib/views/product/widget/build_product_list.dart
@@ -5,6 +5,7 @@ import 'package:flipper/views/product/widget/build_product_row.dart';
 import 'package:flipper/views/stock/stock_viewmodel.dart';
 import 'package:flipper/utils/HexColor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter_text_drawable/flutter_text_drawable.dart';
 import 'package:stacked/stacked.dart';
 
@@ -36,98 +37,122 @@ List<Widget> buildProductList(
         product.name != 'tmp' &&
         product.name != 'Custom Amount') {
       list.add(
-        GestureDetector(
-          onTap: () {
-            if (shouldSeeItem) {
-              model.shouldSeeItemOnly(context, product);
-            } else {
-              model.onSellingItem(context, product);
-            }
-          },
-          onLongPress: () {
-            if (shouldSeeItem) {
-              model.shouldSeeItemOnly(context, product);
-            } else {
-              model.onSellingItem(context, product);
-            }
-          },
-          child: Column(children: <Widget>[
-            ListTile(
-              contentPadding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
-              // leading: callImageBox(context, product),
-              leading: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                width: 58,
-                child: product.picture == null
-                    ? TextDrawable(
-                        backgroundColor: HexColor(product.color),
-                        text: product.name,
-                        isTappable: true,
-                        onTap: null,
-                        boxShape: BoxShape.rectangle,
-                      )
-                    : Image.network(
-                        product.picture,
-                        fit: BoxFit.cover,
-                        frameBuilder: (BuildContext context, Widget child,
-                            int frame, bool wasSynchronouslyLoaded) {
-                          if (wasSynchronouslyLoaded) {
-                            return child;
-                          }
-                          return AnimatedOpacity(
-                            child: child,
-                            opacity: frame == null ? 0 : 1,
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.easeOut,
-                          );
-                        },
-                      ),
-              ),
-              title: Text(
-                product.name,
-                style: const TextStyle(color: Colors.black),
-              ),
-              trailing: ViewModelBuilder<StockViewModel>.reactive(
-                viewModelBuilder: () => StockViewModel(),
-                onModelReady: (StockViewModel stockModel) =>
-                    stockModel.loadStockByProductId(
-                        productId: product.id, context: context),
-                builder: (BuildContext context, StockViewModel stockModel,
-                    Widget child) {
-                  // FIXME: fix showing prices when a product has more than one variant
-                  return stockModel.stock.isEmpty || stockModel.stock.length > 1
-                      ? const Text(
-                          ' Prices',
-                          style: TextStyle(color: Colors.black),
+        Slidable(
+          child: GestureDetector(
+            onTap: () {
+              if (shouldSeeItem) {
+                model.shouldSeeItemOnly(context, product);
+              } else {
+                model.onSellingItem(context, product);
+              }
+            },
+            onLongPress: () {
+              if (shouldSeeItem) {
+                model.shouldSeeItemOnly(context, product);
+              } else {
+                model.onSellingItem(context, product);
+              }
+            },
+            child: Column(children: <Widget>[
+              ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                // leading: callImageBox(context, product),
+                leading: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: 58,
+                  child: product.picture == null
+                      ? TextDrawable(
+                          backgroundColor: HexColor(product.color),
+                          text: product.name,
+                          isTappable: true,
+                          onTap: null,
+                          boxShape: BoxShape.rectangle,
                         )
-                      : Text(
-                          'RWF ' +
-                              stockModel.stock[0].retailPrice
-                                  .toInt()
-                                  .toString(),
-                          style: const TextStyle(color: Colors.black),
-                        );
-                },
+                      : Image.network(
+                          product.picture,
+                          fit: BoxFit.cover,
+                          frameBuilder: (BuildContext context, Widget child,
+                              int frame, bool wasSynchronouslyLoaded) {
+                            if (wasSynchronouslyLoaded) {
+                              return child;
+                            }
+                            return AnimatedOpacity(
+                              child: child,
+                              opacity: frame == null ? 0 : 1,
+                              duration: const Duration(seconds: 1),
+                              curve: Curves.easeOut,
+                            );
+                          },
+                        ),
+                ),
+                title: Text(
+                  product.name,
+                  style: const TextStyle(color: Colors.black),
+                ),
+                trailing: ViewModelBuilder<StockViewModel>.reactive(
+                  viewModelBuilder: () => StockViewModel(),
+                  onModelReady: (StockViewModel stockModel) =>
+                      stockModel.loadStockByProductId(
+                          productId: product.id, context: context),
+                  builder: (BuildContext context, StockViewModel stockModel,
+                      Widget child) {
+                    // FIXME: fix showing prices when a product has more than one variant
+                    return stockModel.stock.isEmpty ||
+                            stockModel.stock.length > 1
+                        ? const Text(
+                            ' Prices',
+                            style: TextStyle(color: Colors.black),
+                          )
+                        : Text(
+                            'RWF ' +
+                                stockModel.stock[0].retailPrice
+                                    .toInt()
+                                    .toString(),
+                            style: const TextStyle(color: Colors.black),
+                          );
+                  },
+                ),
               ),
+              Container(
+                height: 0.5,
+                color: Colors.black26,
+              ),
+            ]),
+          ),
+          actions: <Widget>[
+            IconSlideAction(
+              caption: 'Archive',
+              color: Colors.blue,
+              icon: Icons.archive,
+              onTap: () => false,
             ),
-            Container(
-              height: 0.5,
-              color: Colors.black26,
+            IconSlideAction(
+              caption: 'Share',
+              color: Colors.indigo,
+              icon: Icons.share,
+              onTap: () => false,
             ),
-          ]),
+          ],
+          secondaryActions: <Widget>[
+            IconSlideAction(
+              caption: 'More',
+              color: Colors.black45,
+              icon: Icons.more_horiz,
+              onTap: () => false,
+            ),
+            IconSlideAction(
+              caption: 'Delete',
+              color: Colors.red,
+              icon: Icons.delete,
+              onTap: () => false,
+            ),
+          ],
+          actionPane: const SlidableDrawerActionPane(),
         ),
       );
     }
   }
   //
-  // buildProductRowHeader(
-  //   list: list,
-  //   context: context,
-  //   createButtonName: createButtonName,
-  //   userId: userId,
-  //   type: 'add', //on top of product there should be Add buttom
-  // );
-
   if (!showCreateItemOnTop) {
     buildProductRowHeader(
       list: list,
@@ -141,7 +166,7 @@ List<Widget> buildProductList(
   return list;
 }
 
-callImageBox(BuildContext context, Product product) {
+SizedBox callImageBox(BuildContext context, Product product) {
   if (product.hasPicture) {
     return SizedBox(
       height: MediaQuery.of(context).size.height,
