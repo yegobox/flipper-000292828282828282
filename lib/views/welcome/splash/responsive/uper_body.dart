3400628eff4ff311b156112b7ef9580c71d1574a
Richard Muragijimana <beastar457@gmail.com>
2021-03-28 16:07:41 +0200

    fixing the app
    Former-commit-id: 545789e342b1a07c565a4b9eb70ee51ca646aee5

--- lib/views/welcome/splash/responsive/uper_body.dart
+++ lib/views/welcome/splash/responsive/uper_body.dart
@@ -12,29 +12,30 @@ class UperBody extends StatelessWidget {
         children: <Widget>[
           Container(
             height: 60,
-             decoration: BoxDecoration(
+            decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(21),
               color: Colors.white.withOpacity(1.0),
             ),
-            margin:const EdgeInsets.only(top:120),
+            margin: const EdgeInsets.only(top: 120),
             child: Image.asset('assets/graphics/icon.png'),
           ),
-          
           Container(
-            margin: const EdgeInsets.only(bottom:20),
+            margin: const EdgeInsets.only(bottom: 20),
             child: const Padding(
-              padding: EdgeInsets.only(left:18.0,right: 18.0),
+              padding: EdgeInsets.only(left: 18.0, right: 18.0),
               child: Text(
                 'Flipper',
                 style: TextStyle(color: Colors.white, fontSize: 40),
               ),
             ),
           ),
-          Container(
-             margin: const EdgeInsets.only(bottom:40),
-            child: const Text(
-              'Interact and  grow your business .',
-              style:  TextStyle(color: Colors.white, fontSize: 20),
+          Center(
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 40),
+              child: const Text(
+                'Interact and  grow your business .',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
             ),
           )
         ],
