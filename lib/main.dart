297356bf2d2e612eaa70f858fc19f75a2f1abc5f
Richard Muragijimana <beastar457@gmail.com>
2021-04-12 17:05:45 +0200

    get package that works on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flipper/flipper_app.dart';
+import 'package:flipper_login/login_view.dart';
 // import 'package:flipper/locator.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,7 @@ void main() {
   // setupLocator();
   runApp(FlipperApp());
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -13,13 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
-            toggleableActiveColor: Colors.green
-        ),
+            primarySwatch: Colors.blue, toggleableActiveColor: Colors.green),
         home: Scaffold(
           backgroundColor: Colors.grey[300],
           body: LoginView(),
-        )
-    );
+        ));
   }
 }
