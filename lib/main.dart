6729b0e85de0312f101e7cbe8ae616cdc511f53e
Richard Muragijimana <beastar457@gmail.com>
2021-04-13 08:10:33 +0200

    buggy code

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue, toggleableActiveColor: Colors.green),
-        home: Scaffold(
-          backgroundColor: Colors.grey[300],
-          body: LoginView(),
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue, toggleableActiveColor: Colors.green),
+      home: Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: LoginView(),
+      ),
+    );
   }
 }
