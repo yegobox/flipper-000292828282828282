3030fe640c889c909ceb1b5b4a916b88290de70e
Richard Muragijimana <beastar457@gmail.com>
2021-04-17 04:39:04 +0200

    wrap viewmodel

--- packages/flipper_login/lib/signup_form_view.dart
+++ packages/flipper_login/lib/signup_form_view.dart
@@ -1,52 +1,61 @@
+import 'package:flipper_models/view_models/signup_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 class SignUpFormView extends StatelessWidget {
   SignUpFormView({Key? key}) : super(key: key);
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Text('This is signup form'),
-            Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
+    return ViewModelBuilder<SignupViewModel>.reactive(
+      viewModelBuilder: () => SignupViewModel(),
+      builder: (context, model, child) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: Center(
+            child: Column(
+              children: [
+                Text('This is signup form'),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        // The validator receives the text that the user has entered.
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Validate returns true if the form is valid, or false otherwise.
+                            if (_formKey.currentState!.validate()) {
+                              // If the form is valid, display a snackbar. In the real world,
+                              // you'd often call a server or save the information in a database.
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                      content: Text('Processing Data',
+                                          style:
+                                              TextStyle(color: Colors.black))));
+                            }
+                          },
+                          child: Text('Submit'),
+                        ),
+                      ),
+                    ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Validate returns true if the form is valid, or false otherwise.
-                        if (_formKey.currentState!.validate()) {
-                          // If the form is valid, display a snackbar. In the real world,
-                          // you'd often call a server or save the information in a database.
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                              content: Text('Processing Data',
-                                  style: TextStyle(color: Colors.black))));
-                        }
-                      },
-                      child: Text('Submit'),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
