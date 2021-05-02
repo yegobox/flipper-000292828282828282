d4fdbb97e3be37fd3df40c0766b55c09ed29a709
Richard Muragijimana <beastar457@gmail.com>
2021-05-02 18:53:10 +0200

    step#1
    Former-commit-id: 57fda19e3c15b44677d73788498221cb3ec4c67c

--- packages/flipper_login/lib/signup_login_buttons.dart
+++ packages/flipper_login/lib/signup_login_buttons.dart
@@ -7,8 +7,6 @@ class SignUpLoginButton extends StatelessWidget {
   const SignUpLoginButton({Key? key, this.portrait = false}) : super(key: key);
   final bool portrait;
 
-  void _otp(BuildContext context) {}
-
   void _showModalBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -18,7 +16,7 @@ class SignUpLoginButton extends StatelessWidget {
           padding: EdgeInsets.only(bottom: 0),
           child: Container(
             child: PhoneNumberView(),
-            height: 400,
+            // height: 400,
             decoration: const BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
@@ -35,65 +33,65 @@ class SignUpLoginButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<LoginViewModel>.reactive(
-        viewModelBuilder: () => LoginViewModel(),
-        builder: (context, model, child) {
-          return Container(
-            margin: const EdgeInsets.only(top: 50),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 18.0, right: 18.0, top: 0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20.0)),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 60,
-                      child: FlatButton(
-                        key: Key('createAccountButton'),
-                        onPressed: () {
-                          _showModalBottomSheet(context);
-                        },
-                        color: Colors.blue,
-                        child: Text(
-                          'Create Account',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6!
-                              .copyWith(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding:
-                      const EdgeInsets.only(left: 18.0, right: 18.0, top: 20),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
+      viewModelBuilder: () => LoginViewModel(),
+      builder: (context, model, child) {
+        return Container(
+          margin: const EdgeInsets.only(top: 50),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20.0)),
                   child: SizedBox(
                     width: double.infinity,
                     height: 60,
-                    child: OutlineButton(
-                      color: Colors.blue,
-                      child: Text(
-                        'Sign in',
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6!
-                            .copyWith(color: Colors.blue),
-                      ),
+                    child: FlatButton(
+                      key: Key('createAccountButton'),
                       onPressed: () {
                         _showModalBottomSheet(context);
                       },
+                      color: Colors.blue,
+                      child: Text(
+                        'Create Account',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
-          );
-        });
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.only(left: 18.0, right: 18.0, top: 20),
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(10.0)),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: OutlineButton(
+                    color: Colors.blue,
+                    child: Text(
+                      'Sign in',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6!
+                          .copyWith(color: Colors.blue),
+                    ),
+                    onPressed: () {
+                      _showModalBottomSheet(context);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
