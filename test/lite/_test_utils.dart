ccc7ebef0262fec6f15be791905e478d92f20d9c
Richard Muragijimana <beastar457@gmail.com>
2021-05-03 22:44:07 +0200

    make web test works

--- test/lite/_test_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-Future asyncSleep(int millis) => Future.delayed(Duration(milliseconds: millis));
-
-void deleteDBFile(String path) {
-  if (Directory('$path.cblite2').existsSync()) {
-    Directory('$path.cblite2').delete(recursive: true);
-  }
-}
--- test/view_models/api_test.dart
+++ test/view_models/api_test.dart
@@ -3,7 +3,6 @@ import 'package:flipper_models/models/business.dart';
 import 'package:flipper_models/view_models/startup_viewmodel.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-import 'package:couchbase_lite_dart/couchbase_lite_dart.dart';
 import 'dart:io';
 import 'package:flipper_services/abstractions/api.dart';
 import 'package:mockito/mockito.dart';
