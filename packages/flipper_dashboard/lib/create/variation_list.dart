60e20da8f2279b83141b709eb9f04fcb0b1a715f
Richard Muragijimana <beastar457@gmail.com>
2021-05-06 02:36:45 +0200

    avoid data dups
    Former-commit-id: f6f8f5f67b687c55e02d7f8dbd471cb9bd72f225

--- packages/flipper_dashboard/lib/create/variation_list.dart
+++ packages/flipper_dashboard/lib/create/variation_list.dart
@@ -23,65 +23,63 @@ class VariationList extends StatelessWidget {
     for (var i = 0; i < variations.length; i++) {
       if (variations[i].name != 'temp') {
         list.add(
-          Slidable(
-            child: Center(
-              child: SizedBox(
-                height: 90,
-                width: double.infinity,
-                child: ListView(children: <Widget>[
-                  ListTile(
-                    leading: const Icon(
-                      Icons.dehaze,
-                    ),
-                    subtitle: Text(
-                        '${variations[i].name} \nRWF ${variations[i].retailPrice}'),
-                    trailing: StreamBuilder<Stock>(
-                        stream: ProxyService.api.stockByVariantIdStream(
-                          variantId: variations[i].id,
-                        ),
-                        builder: (context, snapshot) {
-                          if (!snapshot.hasData) {
-                            return Text('....');
-                          }
-                          final Stock? stock = snapshot.data;
-                          return Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              TextButton(
-                                child: Text(
-                                  stock == null || stock.currentStock == 0.0
-                                      ? 'Receive Stock'
-                                      : stock.currentStock.toString() +
-                                          ' in stock',
-                                ),
-                                onPressed: () {
-                                  ProxyService.nav.navigateTo(
-                                    Routes.receiveStock,
-                                    arguments: ReceiveStockArguments(
-                                      variantId: variations[i].id,
-                                    ),
-                                  );
-                                },
-                              ),
-                            ],
-                          );
-                        }),
-                    dense: true,
-                  )
-                ]),
-              ),
+          StreamBuilder<Stock>(
+            stream: ProxyService.api.stockByVariantIdStream(
+              variantId: variations[i].id,
             ),
-            secondaryActions: <Widget>[
-              IconSlideAction(
-                caption: 'Delete',
-                color: Colors.red,
-                icon: Icons.delete,
-                onTap: () {
-                  deleteVariant(variations[i].id);
-                },
-              ),
-            ],
-            actionPane: const SlidableDrawerActionPane(),
+            builder: (context, snapshot) {
+              final Stock? stock = snapshot.data;
+              return Slidable(
+                child: Center(
+                  child: SizedBox(
+                    height: 90,
+                    width: double.infinity,
+                    child: ListView(children: <Widget>[
+                      ListTile(
+                        leading: const Icon(
+                          Icons.dehaze,
+                        ),
+                        subtitle: Text(
+                            '${variations[i].name} \nRWF ${stock != null ? stock.retailPrice : 0.0}'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            TextButton(
+                              child: Text(
+                                stock == null || stock.currentStock == 0.0
+                                    ? 'Receive Stock'
+                                    : stock.currentStock.toString() +
+                                        ' in stock',
+                              ),
+                              onPressed: () {
+                                ProxyService.nav.navigateTo(
+                                  Routes.receiveStock,
+                                  arguments: ReceiveStockArguments(
+                                    variantId: variations[i].id,
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        dense: true,
+                      )
+                    ]),
+                  ),
+                ),
+                secondaryActions: <Widget>[
+                  IconSlideAction(
+                    caption: 'Delete',
+                    color: Colors.red,
+                    icon: Icons.delete,
+                    onTap: () {
+                      deleteVariant(variations[i].id);
+                    },
+                  ),
+                ],
+                actionPane: const SlidableDrawerActionPane(),
+              );
+            },
           ),
         );
       }
