898b779f9bb11c1b5c8a3ae27ff6428bd5b8b607
Richard Muragijimana <beastar457@gmail.com>
2021-05-09 21:38:21 +0200

    integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,10 +1,13 @@
-import 'package:flipper_services/proxy.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:flipper/main.dart' as app;
-import '../test/helpers/test_helpers.dart';
+// flutter drive \
+//   --driver=test_driver/integration_test.dart \
+//   --target=integration_test/app_test.dart -d windows
+//
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -16,15 +19,23 @@ void main() {
       app.main();
       await tester.pumpAndSettle();
 
-// flutter drive \
-//   --driver=test_driver/integration_test.dart \
-//   --target=integration_test/app_test.dart -d windows
-//
-
-//  should start by showing a loading screen before moving to other screen
+      // final loaderScreen = find.byKey(Key('Loader'));
+      // await tester.pumpAndSettle();
+      // expect(loaderScreen, findsOneWidget);
       final loaderView = find.byKey(Key('createAccountButton'));
       await tester.pumpAndSettle();
-      expect(loaderView, findsOneWidget); //for now fake testing.
+      expect(loaderView, findsOneWidget);
+
+      //when login or register should show a model.
+      await tester.tap(find.byKey(Key('createAccountButton')));
+      await tester.pumpAndSettle();
+      // expect(find.text('Welcome To'), findsOneWidget);
+      expect(
+          find.byWidgetPredicate((Widget widget) =>
+              widget is RichText &&
+              widget.text.toPlainText().startsWith('Welcome To')),
+          findsOneWidget);
+      // now fill the form with a number and tap expect to go to otp
     });
   });
 }
