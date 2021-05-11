fba2acc10d3ae1040f9df5d516ec67de748b294b
Richard Muragijimana <beastar457@gmail.com>
2021-05-11 06:53:26 +0200

    unit...

---
+++ test/view_models/save_product_test.dart
@@ -0,0 +1,39 @@
+import 'package:flipper_models/models/product.dart';
+import 'package:flipper_models/models/variation.dart';
+import 'package:flipper_models/view_models/product_viewmodel.dart';
+import 'package:flipper_services/constants.dart';
+import 'package:flipper_services/product_service.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../helpers/test_helpers.dart';
+
+ProductViewModel _getModel() => ProductViewModel();
+void main() {
+  group('Test AddProduct Method', () {
+    late ProductService productService;
+    late Map data;
+    setUp(() {
+      registerServices();
+      productService = getAndRegisterProductService(
+        currentUnit: 'kg',
+        branchId: 'BID',
+        userId: 'UID',
+      );
+      data = productService.product!.toJson();
+      data['name'] = 'product name';
+      getAndRegisterApi(data: data, uri: 'product');
+    });
+    tearDown(() => unregisterServices());
+    test('should save a product when a name is given temp product is not null',
+        () async {
+      final model = _getModel();
+
+      model.setName(name: 'product name');
+      expect(model.product, isA<Product>());
+
+      expect(model.name, 'product name');
+
+      expect(await model.addProduct(mproduct: data), true);
+    });
+  });
+}
