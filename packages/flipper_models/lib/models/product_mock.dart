5a0eddf826c7a485dd37a8c916b20cbb44c0ebf1
Richard Muragijimana <beastar457@gmail.com>
2021-05-13 10:17:29 +0200

    ...

--- packages/flipper_models/lib/models/product_mock.dart
+++ packages/flipper_models/lib/models/product_mock.dart
@@ -1,12 +1,44 @@
 library flipper_models;
 
 import 'product.dart';
-import 'package:uuid/uuid.dart';
 import 'package:flipper_services/locator.dart';
 import 'package:flipper_services/app_service.dart';
 
-// final id = Uuid().v1();
 final AppService _appService = locator<AppService>();
+final customProductMock = new Product(
+  id: '',
+  draft: true,
+  currentUpdate: true,
+  taxId: "XX",
+  imageLocal: false,
+  businessId: _appService.businessId!,
+  name: "Custom Amount",
+  description: "L",
+  active: true,
+  hasPicture: false,
+  table: "products",
+  color: "#e74c3c",
+  supplierId: "XXX",
+  categoryId: "XXX",
+  unit: "kg",
+  channels: [_appService.userid!],
+  createdAt: DateTime.now().toIso8601String(),
+  variants: [
+    AllVariant(
+      name: "Regular",
+      sku: "sku",
+      retailPrice: 0,
+      canTrackingStock: false,
+      supplyPrice: 0,
+      branchId: _appService.branchId!,
+      currentStock: 0,
+      unit: "kg",
+      table: "variants",
+      channels: [_appService.userid!],
+      // updatedAt: DateTime.now().toIso8601String()
+    )
+  ],
+);
 
 final productMock = new Product(
   id: '',
