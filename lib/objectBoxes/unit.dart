1ccd266d46248f620384ccc9cb8ca2f8be53c0d5
Richard Muragijimana <beastar457@gmail.com>
2021-05-19 06:54:48 +0200

    done with experimenting with objectbox and what to consider

--- lib/objectBoxes/unit.dart
+++ lib/objectBoxes/unit.dart
@@ -1,21 +1,59 @@
 import 'package:objectbox/objectbox.dart';
 
+import 'dart:convert';
+
+Unit sunitFromJson(String str) => Unit.fromJson(json.decode(str));
+String sunitToJson(Unit data) => json.encode(data.toJson());
+
+List<Unit> unitFromJson(String str) =>
+    List<Unit>.from(json.decode(str).map((x) => Unit.fromJson(x)));
+
+String unitToJson(List<Unit> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 @Entity()
-class Note {
-  // Each "Entity" needs a unique integer ID property.
-  // Add `@Id()` annotation if its name isn't "id" (case insensitive).
-  int id = 0;
+class Unit {
+  Unit(
+      {required this.id,
+      required this.branchId,
+      required this.name,
+      required this.table,
+      required this.active,
+      required this.channels,
+      this.units,
+      required this.ids});
 
-  String? text;
+  @Id()
+  int ids;
 
-  DateTime date;
+  String id;
+  String branchId;
+  String name;
+  String table;
+  bool active;
+  List<String> channels;
+  List<Map<String, dynamic>>? units;
 
-  @Transient() // Make this field ignored, not stored in the database.
-  int? notPersisted;
+  factory Unit.fromJson(Map<String, dynamic> json) => Unit(
+        id: json["id"],
+        ids: json["ids"],
+        branchId: json["branchId"],
+        name: json["name"],
+        table: json["table"],
+        active: json["active"],
+        channels: List<String>.from(json["channels"].map((x) => x)),
+        units: json["units"] == null
+            ? []
+            : List<Map<String, dynamic>>.from(json["units"].map((x) => x)),
+      );
 
-  // An empty default constructor is needed but you can use optional args.
-  Note({this.text, DateTime? date}) : date = date ?? DateTime.now();
-
-  // Note: just for logs in the examples below(), not needed by ObjectBox.
-  // toString() => 'Note{id: $id, text: $text}';
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "branchId": branchId,
+        "name": name,
+        "table": table,
+        "active": active,
+        "channels": List<dynamic>.from(channels.map((x) => x)),
+        "units": units == null ? [] : List<dynamic>.from(units!.map((x) => x)),
+      };
 }
--- lib/objectbox.g.dart
+++ lib/objectbox.g.dart
@@ -14,25 +14,45 @@ export 'package:objectbox/objectbox.dart'; // so that callers only have to impor
 
 final _entities = <ModelEntity>[
   ModelEntity(
-      id: const IdUid(1, 21686609971235120),
-      name: 'Note',
-      lastPropertyId: const IdUid(3, 6858914786176429036),
+      id: const IdUid(2, 1085632613906874556),
+      name: 'Unit',
+      lastPropertyId: const IdUid(7, 3458552990229191164),
       flags: 0,
       properties: <ModelProperty>[
         ModelProperty(
-            id: const IdUid(1, 9165922652151926835),
-            name: 'id',
+            id: const IdUid(1, 3075717765372783850),
+            name: 'ids',
             type: 6,
             flags: 1),
         ModelProperty(
-            id: const IdUid(2, 7435018457391551185),
-            name: 'text',
+            id: const IdUid(2, 8389873262721696551),
+            name: 'id',
             type: 9,
             flags: 0),
         ModelProperty(
-            id: const IdUid(3, 6858914786176429036),
-            name: 'date',
-            type: 10,
+            id: const IdUid(3, 3998032434978306868),
+            name: 'branchId',
+            type: 9,
+            flags: 0),
+        ModelProperty(
+            id: const IdUid(4, 827493739366445200),
+            name: 'name',
+            type: 9,
+            flags: 0),
+        ModelProperty(
+            id: const IdUid(5, 6652315236723266449),
+            name: 'table',
+            type: 9,
+            flags: 0),
+        ModelProperty(
+            id: const IdUid(6, 7937019431931046186),
+            name: 'active',
+            type: 1,
+            flags: 0),
+        ModelProperty(
+            id: const IdUid(7, 3458552990229191164),
+            name: 'channels',
+            type: 30,
             flags: 0)
       ],
       relations: <ModelRelation>[],
@@ -43,47 +63,66 @@ final _entities = <ModelEntity>[
 ModelDefinition getObjectBoxModel() {
   final model = ModelInfo(
       entities: _entities,
-      lastEntityId: const IdUid(1, 21686609971235120),
+      lastEntityId: const IdUid(2, 1085632613906874556),
       lastIndexId: const IdUid(0, 0),
       lastRelationId: const IdUid(0, 0),
       lastSequenceId: const IdUid(0, 0),
-      retiredEntityUids: const [],
+      retiredEntityUids: const [21686609971235120],
       retiredIndexUids: const [],
-      retiredPropertyUids: const [],
+      retiredPropertyUids: const [
+        9165922652151926835,
+        7435018457391551185,
+        6858914786176429036
+      ],
       retiredRelationUids: const [],
       modelVersion: 5,
       modelVersionParserMinimum: 5,
       version: 1);
 
   final bindings = <Type, EntityDefinition>{
-    Note: EntityDefinition<Note>(
+    Unit: EntityDefinition<Unit>(
         model: _entities[0],
-        toOneRelations: (Note object) => [],
-        toManyRelations: (Note object) => {},
-        getId: (Note object) => object.id,
-        setId: (Note object, int id) {
-          object.id = id;
+        toOneRelations: (Unit object) => [],
+        toManyRelations: (Unit object) => {},
+        getId: (Unit object) => object.ids,
+        setId: (Unit object, int id) {
+          object.ids = id;
         },
-        objectToFB: (Note object, fb.Builder fbb) {
-          final textOffset =
-              object.text == null ? null : fbb.writeString(object.text!);
-          fbb.startTable(4);
-          fbb.addInt64(0, object.id);
-          fbb.addOffset(1, textOffset);
-          fbb.addInt64(2, object.date.millisecondsSinceEpoch);
+        objectToFB: (Unit object, fb.Builder fbb) {
+          final idOffset = fbb.writeString(object.id);
+          final branchIdOffset = fbb.writeString(object.branchId);
+          final nameOffset = fbb.writeString(object.name);
+          final tableOffset = fbb.writeString(object.table);
+          final channelsOffset = fbb.writeList(
+              object.channels.map(fbb.writeString).toList(growable: false));
+          fbb.startTable(8);
+          fbb.addInt64(0, object.ids);
+          fbb.addOffset(1, idOffset);
+          fbb.addOffset(2, branchIdOffset);
+          fbb.addOffset(3, nameOffset);
+          fbb.addOffset(4, tableOffset);
+          fbb.addBool(5, object.active);
+          fbb.addOffset(6, channelsOffset);
           fbb.finish(fbb.endTable());
-          return object.id;
+          return object.ids;
         },
         objectFromFB: (Store store, ByteData fbData) {
           final buffer = fb.BufferContext(fbData);
           final rootOffset = buffer.derefObject(0);
 
-          final object = Note(
-              text: const fb.StringReader()
-                  .vTableGetNullable(buffer, rootOffset, 6),
-              date: DateTime.fromMillisecondsSinceEpoch(
-                  const fb.Int64Reader().vTableGet(buffer, rootOffset, 8, 0)))
-            ..id = const fb.Int64Reader().vTableGet(buffer, rootOffset, 4, 0);
+          final object = Unit(
+              id: const fb.StringReader().vTableGet(buffer, rootOffset, 6, ''),
+              branchId:
+                  const fb.StringReader().vTableGet(buffer, rootOffset, 8, ''),
+              name:
+                  const fb.StringReader().vTableGet(buffer, rootOffset, 10, ''),
+              table:
+                  const fb.StringReader().vTableGet(buffer, rootOffset, 12, ''),
+              active: const fb.BoolReader()
+                  .vTableGet(buffer, rootOffset, 14, false),
+              channels: const EagerListReader<String>(fb.StringReader())
+                  .vTableGet(buffer, rootOffset, 16, []),
+              ids: const fb.Int64Reader().vTableGet(buffer, rootOffset, 4, 0));
 
           return object;
         })
@@ -92,14 +131,27 @@ ModelDefinition getObjectBoxModel() {
   return ModelDefinition(model, bindings);
 }
 
-/// [Note] entity fields to define ObjectBox queries.
-class Note_ {
-  /// see [Note.id]
-  static final id = QueryIntegerProperty<Note>(_entities[0].properties[0]);
+/// [Unit] entity fields to define ObjectBox queries.
+class Unit_ {
+  /// see [Unit.ids]
+  static final ids = QueryIntegerProperty<Unit>(_entities[0].properties[0]);
 
-  /// see [Note.text]
-  static final text = QueryStringProperty<Note>(_entities[0].properties[1]);
+  /// see [Unit.id]
+  static final id = QueryStringProperty<Unit>(_entities[0].properties[1]);
 
-  /// see [Note.date]
-  static final date = QueryIntegerProperty<Note>(_entities[0].properties[2]);
+  /// see [Unit.branchId]
+  static final branchId = QueryStringProperty<Unit>(_entities[0].properties[2]);
+
+  /// see [Unit.name]
+  static final name = QueryStringProperty<Unit>(_entities[0].properties[3]);
+
+  /// see [Unit.table]
+  static final table = QueryStringProperty<Unit>(_entities[0].properties[4]);
+
+  /// see [Unit.active]
+  static final active = QueryBooleanProperty<Unit>(_entities[0].properties[5]);
+
+  /// see [Unit.channels]
+  static final channels =
+      QueryStringVectorProperty<Unit>(_entities[0].properties[6]);
 }
