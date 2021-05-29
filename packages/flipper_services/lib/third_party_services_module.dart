29429ba774fa59b718c06934bec4d03458b7cb3c
Richard Muragijimana <beastar457@gmail.com>
2021-05-29 15:29:38 +0200

    merge
    Former-commit-id: 9e27efa1f44196f026d83b1b1310f6fabdad879e

--- packages/flipper_services/lib/third_party_services_module.dart
+++ packages/flipper_services/lib/third_party_services_module.dart
@@ -31,11 +31,7 @@ import 'keypad_service.dart';
 import 'local_storage.dart';
 import 'location_service.dart';
 import 'package:universal_platform/universal_platform.dart';
-<<<<<<< HEAD
-=======
-import 'package:image_picker/image_picker.dart';
->>>>>>> web
-// import 'package:couchbase_lite_dart/couchbase_lite_dart.dart';
+import 'package:couchbase_lite_dart/couchbase_lite_dart.dart';
 
 // final Database db = Database("db");
 final dynamic db = {}; //fake db it is not going to be used on web anyway!
