d26022571e1f41e913f779d001e143e78df6b80e
Richard Muragijimana <beastar457@gmail.com>
2021-07-01 16:40:38 +0200

    make design looks the same follow figma! in future

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:universal_platform/universal_platform.dart';
-import 'package:flipper_services/proxy.dart';
 
 final isWindows = UniversalPlatform.isWindows;
 final isMacOs = UniversalPlatform.isMacOS;
