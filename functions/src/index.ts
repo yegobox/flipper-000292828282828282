d7617b0a2e8397da598f1a79e38740a636843c51
Richard Muragijimana <beastar457@gmail.com>
2021-08-21 13:17:33 +0200

    work on serious notification system
    Former-commit-id: fde4b874bbc9b638fc24eeb7d97f3ced4f9f6ec4

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,22 @@
 import * as functions from "firebase-functions";
-
+const axios = require('axios');
+// const cors = require('cors')({ origin: true });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+
+exports.onMessageScheduled = functions.firestore
+    .document('messages/{userId}')
+    .onCreate(async (snapshot, context) => {
+        axios.post('https://apihub.yegobox.com/v2/api/sendToAll', {
+            title: snapshot.data().title,
+            message: snapshot.data().message,
+            userId: snapshot.id
+        })
+            .then((r: any) => {
+                console.log(r);
+                console.log('Sent the notification to all', snapshot.data().title, snapshot.data().message);
+            })
+            .catch(function () {
+                console.log('Failed to send notification');
+            });
+    });
