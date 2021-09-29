f10274b52ae9685d958e83f0a37510964643c167
Richard Muragijimana <beastar457@gmail.com>
2021-09-29 11:00:36 +0200

    flipper_web

---
+++ packages/flipper_web/lib/main.dart
@@ -0,0 +1,272 @@
+import 'package:flipper_web/screens/basic_setup.dart';
+import 'package:flipper_web/screens/login/login_screen.dart';
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
+
+import 'package:provider/provider.dart';
+import 'package:system_theme/system_theme.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter_acrylic/flutter_acrylic.dart' as flutter_acrylic;
+import 'package:url_strategy/url_strategy.dart';
+
+import 'screens/settings.dart';
+
+import 'theme.dart';
+
+const String appTitle = '';
+
+late bool darkMode;
+
+/// Checks if the current environment is a desktop environment.
+bool get isDesktop {
+  if (kIsWeb) return false;
+  return [
+    TargetPlatform.windows,
+    TargetPlatform.linux,
+    TargetPlatform.macOS,
+  ].contains(defaultTargetPlatform);
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  setPathUrlStrategy();
+
+  // The platforms the plugin support (01/04/2021 - DD/MM/YYYY):
+  //   - Windows
+  //   - Web
+  //   - Android
+  if (defaultTargetPlatform == TargetPlatform.windows ||
+      defaultTargetPlatform == TargetPlatform.android ||
+      kIsWeb) {
+    darkMode = await SystemTheme.darkMode;
+    await SystemTheme.accentInstance.load();
+  } else {
+    darkMode = true;
+  }
+  if (!kIsWeb &&
+      [TargetPlatform.windows, TargetPlatform.linux]
+          .contains(defaultTargetPlatform)) {
+    await flutter_acrylic.Acrylic.initialize();
+  }
+
+  runApp(const MyApp());
+
+  if (isDesktop) {
+    doWhenWindowReady(() {
+      final win = appWindow;
+      win.minSize = const Size(410, 540);
+      win.size = const Size(755, 545);
+      win.alignment = Alignment.center;
+      win.title = appTitle;
+      win.show();
+    });
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AppTheme(),
+      builder: (context, _) {
+        final appTheme = context.watch<AppTheme>();
+        return FluentApp(
+          title: appTitle,
+          themeMode: appTheme.mode,
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/',
+          routes: {
+            '/': (_) => Login(
+                  title: 'flipper',
+                )
+          },
+          theme: ThemeData(
+            accentColor: appTheme.color,
+            brightness: appTheme.mode == ThemeMode.system
+                ? darkMode
+                    ? Brightness.dark
+                    : Brightness.light
+                : appTheme.mode == ThemeMode.dark
+                    ? Brightness.dark
+                    : Brightness.light,
+            visualDensity: VisualDensity.standard,
+            focusTheme: FocusThemeData(
+              glowFactor: is10footScreen() ? 2.0 : 0.0,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DashBoard extends StatefulWidget {
+  const DashBoard({Key? key}) : super(key: key);
+
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
+  bool value = false;
+
+  int index = 0;
+
+  final colorsController = ScrollController();
+  final settingsController = ScrollController();
+
+  @override
+  void dispose() {
+    colorsController.dispose();
+    settingsController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final appTheme = context.watch<AppTheme>();
+    return NavigationView(
+      pane: NavigationPane(
+        selected: index,
+        onChanged: (i) => setState(() => index = i),
+        header: Container(
+          height: kOneLineTileHeight,
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: const FlutterLogo(
+            style: FlutterLogoStyle.horizontal,
+            size: 100,
+          ),
+        ),
+        // displayMode: PaneDisplayMode.compact,
+        displayMode: appTheme.displayMode,
+        // TODOput back the  appTheme.displayMode, after setting up the compact mode
+        // PaneDisplayMode
+        indicatorBuilder: ({
+          required BuildContext context,
+          int? index,
+          required List<Offset> Function() offsets,
+          required List<Size> Function() sizes,
+          required Axis axis,
+          required Widget child,
+        }) {
+          if (index == null) return child;
+          assert(debugCheckHasFluentTheme(context));
+          final theme = NavigationPaneTheme.of(context);
+          switch (appTheme.indicator) {
+            case NavigationIndicators.end:
+              return EndNavigationIndicator(
+                index: index,
+                offsets: offsets,
+                sizes: sizes,
+                child: child,
+                color: theme.highlightColor,
+                curve: theme.animationCurve ?? Curves.linear,
+                axis: axis,
+              );
+            case NavigationIndicators.sticky:
+              return NavigationPane.defaultNavigationIndicator(
+                index: index,
+                context: context,
+                offsets: offsets,
+                sizes: sizes,
+                axis: axis,
+                child: child,
+              );
+            default:
+              return NavigationIndicator(
+                index: index,
+                offsets: offsets,
+                sizes: sizes,
+                child: child,
+                color: theme.highlightColor,
+                curve: theme.animationCurve ?? Curves.linear,
+                axis: axis,
+              );
+          }
+        },
+        items: [
+          PaneItemSeparator(),
+          PaneItem(
+            icon: Icon(
+              appTheme.displayMode == PaneDisplayMode.top
+                  ? FluentIcons.more
+                  : FluentIcons.more_vertical,
+            ),
+            title: const Text('Others'),
+          ),
+        ],
+        autoSuggestBox: AutoSuggestBox<String>(
+          controller: TextEditingController(),
+          items: const ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+        ),
+        autoSuggestBoxReplacement: const Icon(FluentIcons.search),
+        footerItems: [
+          PaneItemSeparator(),
+          PaneItem(
+            icon: const Icon(FluentIcons.settings),
+            title: const Text('Settings'),
+          ),
+        ],
+      ),
+      content: NavigationBody(index: index, children: [
+        BasicSetUp(),
+        Settings(controller: settingsController),
+      ]),
+    );
+  }
+}
+
+class WindowButtons extends StatelessWidget {
+  const WindowButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasFluentTheme(context));
+    assert(debugCheckHasFluentLocalizations(context));
+    final ThemeData theme = FluentTheme.of(context);
+    final buttonColors = WindowButtonColors(
+      iconNormal: theme.inactiveColor,
+      iconMouseDown: theme.inactiveColor,
+      iconMouseOver: theme.inactiveColor,
+      mouseOver: ButtonThemeData.buttonColor(
+          theme.brightness, {ButtonStates.hovering}),
+      mouseDown: ButtonThemeData.buttonColor(
+          theme.brightness, {ButtonStates.pressing}),
+    );
+    final closeButtonColors = WindowButtonColors(
+      mouseOver: Colors.red,
+      mouseDown: Colors.red.dark,
+      iconNormal: theme.inactiveColor,
+      iconMouseOver: Colors.red.basedOnLuminance(),
+      iconMouseDown: Colors.red.dark.basedOnLuminance(),
+    );
+    return Row(
+      children: [
+        Tooltip(
+          message: FluentLocalizations.of(context).minimizeWindowTooltip,
+          child: MinimizeWindowButton(colors: buttonColors),
+        ),
+        Tooltip(
+          message: FluentLocalizations.of(context).restoreWindowTooltip,
+          child: WindowButton(
+            colors: buttonColors,
+            iconBuilder: (context) {
+              if (appWindow.isMaximized) {
+                return RestoreIcon(color: context.iconColor);
+              }
+              return MaximizeIcon(color: context.iconColor);
+            },
+            onPressed: appWindow.maximizeOrRestore,
+          ),
+        ),
+        Tooltip(
+          message: FluentLocalizations.of(context).closeWindowTooltip,
+          child: CloseWindowButton(colors: closeButtonColors),
+        ),
+      ],
+    );
+  }
+}
