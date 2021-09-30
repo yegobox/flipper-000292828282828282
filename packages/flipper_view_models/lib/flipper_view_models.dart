c1c3424a3b5b85ae3ab8baa8738ac238f1bfac92
Richard Muragijimana <beastar457@gmail.com>
2021-09-30 09:44:55 +0200

    1/5

--- packages/flipper_view_models/lib/flipper_view_models.dart
+++ packages/flipper_view_models/lib/flipper_view_models.dart
@@ -1,7 +1,3 @@
-library flipper_view_models;
+// library flipper_view_models;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'login_viewmodel.dart';
