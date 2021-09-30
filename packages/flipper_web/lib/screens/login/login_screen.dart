7c866f7a339afb83042a86777085ce719e1542f8
Richard Muragijimana <beastar457@gmail.com>
2021-09-30 08:06:41 +0200

    extract web component
    Former-commit-id: 08b81f48ecd6636c81b6ceb91cdea3233dcf512a

--- packages/flipper_web/lib/screens/login/login_screen.dart
+++ packages/flipper_web/lib/screens/login/login_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flipper_web/core/constants/color_constants.dart';
-import 'package:flipper_web/core/widgets/app_button_widget.dart';
 import 'package:flipper_web/core/widgets/input_widget.dart';
 import 'package:flipper_web/main.dart';
-import 'package:flipper_web/screens/home/home_screen.dart';
-import 'package:flipper_web/screens/login/components/slider_widget.dart';
-
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:stacked/stacked.dart';
 import 'package:flutter/material.dart';
+import 'package:common_codes/src/login_viewmodel.dart';
+
+import 'components/slider_widget.dart';
 
 class Login extends StatefulWidget {
   Login({required this.title});
@@ -43,244 +44,71 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        fit: StackFit.loose,
-        children: <Widget>[
-          Row(
+    return ViewModelBuilder<LoginViewModel>.reactive(
+      viewModelBuilder: () => LoginViewModel(),
+      builder: (context, model, child) {
+        return Scaffold(
+          // backgroundColor: Colors.white,
+          body: Stack(
+            fit: StackFit.loose,
             children: <Widget>[
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width / 2,
-                color: Colors.white,
-                child: SliderWidget(),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height,
-                width: MediaQuery.of(context).size.width / 2,
-                color: bgColor,
-                child: Center(
-                  child: Card(
-                    //elevation: 5,
+              Row(
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width / 2,
+                    // color: Colors.white,
+                    child: SliderWidget(),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width / 2,
                     color: bgColor,
-                    child: Container(
-                      padding: EdgeInsets.all(42),
-                      width: MediaQuery.of(context).size.width / 3.6,
-                      height: MediaQuery.of(context).size.height / 1.2,
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            height: 60,
-                          ),
-                          Image.asset("assets/logo/logo_icon.png", scale: 3),
-                          SizedBox(height: 24.0),
-
-                          Flexible(
-                            child: Stack(
-                              children: [
-                                SlideTransition(
-                                  position:
-                                      _animationController!.drive(tweenRight),
-                                  child: Stack(
-                                      fit: StackFit.loose,
-                                      clipBehavior: Clip.none,
-                                      children: [
-                                        _loginScreen(context),
-                                      ]),
+                    child: Center(
+                      child: Card(
+                        //elevation: 5,
+                        color: bgColor,
+                        child: Container(
+                          padding: EdgeInsets.all(42),
+                          width: MediaQuery.of(context).size.width / 3.6,
+                          height: MediaQuery.of(context).size.height / 1.2,
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(
+                                height: 60,
+                              ),
+                              Image.asset("assets/logo/logo_icon.png",
+                                  scale: 3),
+                              SizedBox(height: 24.0),
+                              Flexible(
+                                child: Stack(
+                                  children: [
+                                    SlideTransition(
+                                      position: _animationController!
+                                          .drive(tweenRight),
+                                      child: Stack(
+                                        fit: StackFit.loose,
+                                        clipBehavior: Clip.none,
+                                        children: [
+                                          _loginScreen(context),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                                SlideTransition(
-                                  position:
-                                      _animationController!.drive(tweenLeft),
-                                  child: Stack(
-                                      fit: StackFit.loose,
-                                      clipBehavior: Clip.none,
-                                      children: [
-                                        _registerScreen(context),
-                                      ]),
-                                ),
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-
-                          //Flexible(
-                          //  child: SlideTransition(
-                          //    position: _animationController!.drive(tweenLeft),
-                          //    child: Stack(
-                          //        fit: StackFit.loose,
-                          //        clipBehavior: Clip.none,
-                          //        children: [
-                          //          _registerScreen(context),
-                          //        ]),
-                          //  ),
-                          //),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  Container _registerScreen(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    return Container(
-      width: double.infinity,
-      constraints: BoxConstraints(
-        minHeight: MediaQuery.of(context).size.height - 0.0,
-      ),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            InputWidget(
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (String? value) {
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              onChanged: (String? value) {
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              validator: (String? value) {
-                return (value != null && value.contains('@'))
-                    ? 'Do not use the @ char.'
-                    : null;
-              },
-
-              topLabel: "Name",
-
-              hintText: "Enter Name",
-              // prefixIcon: FlutterIcons.chevron_left_fea,
-            ),
-            SizedBox(height: 8.0),
-            InputWidget(
-              keyboardType: TextInputType.emailAddress,
-              onSaved: (String? value) {
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              onChanged: (String? value) {
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              validator: (String? value) {
-                if (value == null) {
-                  return "Please enter email";
-                }
-                // regex to validate a Afghanistan, Albania, Algeria, Andorra, Angola, Antigua & Deps, Argentina, Armenia, Australia, Austria,
-                //Azerbaijan, Bahamas, Bahrain, Bangladesh, Barbados, Belarus,
-                //Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia Herzegovina,
-                //Botswana, Brazil, Brunei, Bulgaria, Burkina, Burundi,
-                //Cambodia, Cameroon, Canada, Cape Verde, Central African Rep,
-                //Chad, Chile, China, Colombia, Comoros, Congo, Congo {Democratic Rep},
-                //Costa Rica, Croatia, Cuba, Cyprus, Czech Republic, Denmark, Djibouti, Dominica,
-                //Dominican Republic, East Timor, Ecuador, Egypt, El Salvador, Equatorial Guinea,
-                //Eritrea, Estonia, Ethiopia, Fiji, Finland, France, Gabon, Gambia, Georgia, Germany,
-                //Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana, Haiti, Honduras,
-                //Hungary, Iceland, India, Indonesia, Iran, Iraq, Ireland {Republic}, Israel, Italy,
-                //Ivory Coast, Jamaica, Japan, Jordan, Kazakhstan, Kenya, Kiribati, Korea North,
-                //Korea South, Kosovo, Kuwait, Kyrgyzstan, Laos, Latvia, Lebanon, Lesotho, Liberia,
-                //Libya, Liechtenstein, Lithuania, Luxembourg, Macedonia, Madagascar, Malawi, Malaysia,
-                //Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia,
-                //Moldova, Monaco, Mongolia, Montenegro, Morocco, Mozambique, Myanmar, {Burma}, Namibia,
-                //Nauru, Nepal, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, Norway, Oman, Pakistan,
-                //Palau, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal, Qatar,
-                //Romania, Russian Federation, Rwanda, St Kitts & Nevis, St Lucia, Saint Vincent & the Grenadines,
-                //Samoa, San Marino, Sao Tome & Principe, Saudi Arabia, Senegal, Serbia, Seychelles,
-                //Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa,
-                //South Sudan, Spain, Sri Lanka, Sudan, Suriname, Swaziland, Sweden, Switzerland,
-                //Syria, Taiwan, Tajikistan, Tanzania, Thailand, Togo, Tonga, Trinidad & Tobago, Tunisia,
-                //Turkey, Turkmenistan, Tuvalu, Uganda, Ukraine, United Arab Emirates, United Kingdom,
-                //United States, Uruguay, Uzbekistan, Vanuatu, Vatican City, Venezuela, Vietnam, Yemen,
-                //Zambia, Zimbabwe, phone number
-
-                if (!RegExp(r'^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$')
-                    .hasMatch(value)) {
-                  return 'Please enter a valid phone number';
-                }
-              },
-
-              topLabel: "Phone number",
-
-              hintText: "Enter Phone number",
-              // prefixIcon: FlutterIcons.chevron_left_fea,
-            ),
-            SizedBox(height: 8.0),
-            SizedBox(height: 24.0),
-            AppButton(
-              type: ButtonType.PRIMARY,
-              text: "Sign Up",
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                  );
-                }
-              },
-            ),
-            SizedBox(height: 24.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: isChecked,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked = value!;
-                        });
-                      },
-                    ),
-                    Text("Remember Me")
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 24.0),
-            Center(
-              child: Wrap(
-                runAlignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account?",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (_isMoved) {
-                        _animationController!.reverse();
-                      } else {
-                        _animationController!.forward();
-                      }
-                      _isMoved = !_isMoved;
-                    },
-                    child: Text("Sign In",
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                            fontWeight: FontWeight.w400, color: greenColor)),
                   )
                 ],
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 
@@ -348,9 +176,8 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
               // prefixIcon: FlutterIcons.chevron_left_fea,
             ),
             SizedBox(height: 8.0),
-            AppButton(
-              type: ButtonType.PRIMARY,
-              text: "Sign In",
+            Button(
+              child: Text("Sign In"),
               onPressed: () {
                 if (_formKey.currentState!.validate()) {
                   Navigator.push(
@@ -361,67 +188,6 @@ class _LoginState extends State<Login> with SingleTickerProviderStateMixin {
               },
             ),
             SizedBox(height: 24.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Checkbox(
-                      value: isChecked,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked = value!;
-                        });
-                      },
-                    ),
-                    Text("Remember Me")
-                  ],
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: Text(
-                    "Forget Password?",
-                    textAlign: TextAlign.right,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2!
-                        .copyWith(color: greenColor),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 24.0),
-            Center(
-              child: Wrap(
-                runAlignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an account yet?",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText1!
-                        .copyWith(fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      if (_isMoved) {
-                        _animationController!.reverse();
-                      } else {
-                        _animationController!.forward();
-                      }
-                      _isMoved = !_isMoved;
-                    },
-                    child: Text("Sign up",
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                            fontWeight: FontWeight.w400, color: greenColor)),
-                  )
-                ],
-              ),
-            ),
           ],
         ),
       ),
