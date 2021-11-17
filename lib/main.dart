c6e37b554e8f8b6f7d40abd571f631e883930828
Richard Muragijimana <beastar457@gmail.com>
2021-11-17 09:08:24 +0200

    add support for sentry
    Former-commit-id: 3b550a0cfc4a53702cfe1d5b28cb73b8d5adb628

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 // import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:stacked_themes/stacked_themes.dart';
 // import 'package:awesome_notifications/awesome_notifications.dart';
@@ -80,6 +81,15 @@ void main() async {
   await ObjectBoxApi.getDir(dbName: 'db_1');
 
   runZonedGuarded<Future<void>>(() async {
+    await SentryFlutter.init(
+      (options) {
+        options.dsn =
+            'https://f3b8abd190f84fa0abdb139178362bc2@o205255.ingest.sentry.io/6067680';
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+        // We recommend adjusting this value in production.
+        options.tracesSampleRate = 1.0;
+      },
+    );
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
         statusBarColor: Colors.black,
@@ -92,7 +102,8 @@ void main() async {
     //   SystemUiMode.leanBack,
     // );
     runApp(FlipperApp());
-  }, (error, stack) {
+  }, (error, stack) async {
+    await Sentry.captureException(error, stackTrace: stack);
     if (!isWindows) {
       FirebaseCrashlytics.instance.recordError(error, stack);
     }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   nb_utils
   objectbox_flutter_libs
   printing
+  sentry_flutter
   system_theme
   url_launcher_windows
 )
