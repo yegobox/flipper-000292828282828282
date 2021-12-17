622974ffb89a65cfe34658072ec66e8bf513621a
Richard Muragijimana <beastar457@gmail.com>
2021-12-17 09:51:46 +0200

    sync improved
    Former-commit-id: 25d38e6a50ba2e797fa1cbd3c3cd8fbb8afd3920

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   connectivity_plus_windows
   flutter_acrylic
   nb_utils
-  objectbox_flutter_libs
+  objectbox_sync_flutter_libs
   printing
   sentry_flutter
   system_theme
