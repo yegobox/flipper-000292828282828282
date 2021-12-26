72b4c1e8d62e9f0e1fd2c922f370e661ebf6847d
Richard Muragijimana <beastar457@gmail.com>
2021-12-26 12:18:54 +0200

    about to improve the login on desktop

--- lib/io_init.dart
+++ lib/io_init.dart
@@ -1,5 +1,19 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flipper_services/objectbox_api.dart';
 
 Future<void> initDb() async {
   await ObjectBoxApi.getDir(dbName: 'db_1');
+  if (Platform.isWindows) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyCpnbw9i23T0237jgd2ladtPgsGYbmioqA',
+      appId: '1:672237316015:web:e289bfb5c92506c1c2715a',
+      messagingSenderId: 'G-88GKL70K3K',
+      projectId: 'yegobox-2ee43',
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
 }
