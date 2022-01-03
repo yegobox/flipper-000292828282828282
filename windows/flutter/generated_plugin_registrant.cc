794f7a5353bbce4ecb6e389e5a768cafe8b79821
Richard Muragijimana <beastar457@gmail.com>
2022-01-03 12:51:36 +0200

    getting chat to comiple on web
    Former-commit-id: 1a7149da4211c5ea6f70df4daaf7bf474cca73e3

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,7 +7,6 @@
 #include "generated_plugin_registrant.h"
 
 #include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
-#include <flutter_acrylic/flutter_acrylic_plugin.h>
 #include <nb_utils/nb_utils_plugin.h>
 #include <objectbox_sync_flutter_libs/objectbox_sync_flutter_libs_plugin.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
@@ -17,8 +16,6 @@
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   ConnectivityPlusWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  FlutterAcrylicPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterAcrylicPlugin"));
   NbUtilsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NbUtilsPlugin"));
   ObjectboxSyncFlutterLibsPluginRegisterWithRegistrar(
