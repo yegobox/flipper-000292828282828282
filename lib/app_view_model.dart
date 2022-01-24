ec5b3977ce2a09c137da969e11ab18a666fee256
Richard Muragijimana <beastar457@gmail.com>
2022-01-25 00:51:04 +0200

    add the gate code back in its place
    Former-commit-id: 222f1891d4fb83a4cd28777b57c71b0f450a838f

--- lib/app_view_model.dart
+++ lib/app_view_model.dart
@@ -1,10 +1,8 @@
 import 'package:flipper_dashboard/setting_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:localize/gen_l10n/flipper_localizations.dart';
 
 class AppViewModel extends SettingViewModel {
   void setLocale(Locale locale) {
-    if (!AppLocalizations.supportedLocales.contains(locale)) return;
     klocale = locale;
     notifyListeners();
   }
