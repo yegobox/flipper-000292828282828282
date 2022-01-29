1013d64d883199d0df8aa5536ffe963c307618e4
Richard Muragijimana <beastar457@gmail.com>
2022-01-29 08:35:45 +0200

    a more secure way to not share the key publicy
    Former-commit-id: 9b5026e22735912626668c3850b662fe27a02d6a

--- packages/flipper_routing/lib/routes.dart
+++ packages/flipper_routing/lib/routes.dart
@@ -249,3 +249,4 @@ class AppSetup {
 // FIXMELaunching lib\main.dart on Android SDK built for x86 in debug mode...
 // lib\main.dart:1
 // Shrinking has been disabled for this build due to deferred components. Shrinking is not available for multi-apk applications. This limitation is expected to be removed when Gradle plugin 4.2+ is available in Flutter.
+// openssl base64 -in key.jks -out ke
