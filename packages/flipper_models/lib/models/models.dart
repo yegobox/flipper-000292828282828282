cc02169cc360b94fc807c43d3506a73fbb2a6a78
Richard Muragijimana <beastar457@gmail.com>
2022-01-30 08:23:34 +0200

    attempt to fix lagging when loggin  and with sync enabled

--- packages/flipper_models/lib/models/models.dart
+++ packages/flipper_models/lib/models/models.dart
@@ -1,7 +1,8 @@
 export 'package:flipper_models/models/io/business.dart'
     if (dart.library.html) 'package:flipper_models/models/html/business.dart';
 
-/// a model used to store business data in a model that has no sy
+/// a model used to store business data in a model that has no sync
+/// as sync delay login and when no internet it assume no business is local
 export 'package:flipper_models/models/io/l_business.dart'
     if (dart.library.html) 'package:flipper_models/models/html/l_business.dart';
 
