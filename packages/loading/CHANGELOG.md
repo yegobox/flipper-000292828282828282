b0e4e0d51b7b5e90c54d8a93925b689e2ecd9ea3
Richard Muragijimana <beastar457@gmail.com>
2022-02-06 20:43:57 +0200

    add root package

--- packages/loading/CHANGELOG.md
+++ packages/loading/CHANGELOG.md
@@ -1,3 +1,4 @@
+## [2.0.0] - updates.
 ## [0.0.1] - initial release and pub.
 ## [0.0.2] - change class name.
 ## [1.0.0] - work out.
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,12 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
-#include <objectbox_sync_flutter_libs/objectbox_sync_flutter_libs_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
-  ObjectboxSyncFlutterLibsPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("ObjectboxSyncFlutterLibsPlugin"));
 }
