2ba21977211b7acb67a2dbb78e075238fb23d7dc
Richard Muragijimana <beastar457@gmail.com>
2022-02-06 11:10:06 +0200

    compiling
    Former-commit-id: be8886f652132e37f52932b953191d5a54d1701e

--- lib/exception.dart
+++
@@ -1,7 +0,0 @@
-class IllegalOperationException implements Exception {
-  final String cause;
-
-  IllegalOperationException(this.cause);
-
-  String errMsg() => cause;
-}
--- packages/flipper_map/lib/flipper_map.dart
+++
@@ -1,5 +0,0 @@
-library flipper_map;
-
-export 'map_view.dart';
-export 'mapbottompill.dart';
-export 'mapuserbadge.dart';
