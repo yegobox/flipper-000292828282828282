024cb8f604814dd378fa4c33e72c51dcd0c9022c
Richard Muragijimana <beastar457@gmail.com>
2022-02-10 08:35:52 +0200

    closes #108
    Former-commit-id: 4f12baffa55e30f1a6a41470d682ed65012b8666

---
+++ packages/flipper_data_table/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flipper_data_table/page/sortable_page.dart';
+// import 'package:flipper_data_table/widget/tabbar_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_ui/google_ui.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Daily Report';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: GThemeGenerator.generate(),
+        darkTheme: GThemeGenerator.generateDark(),
+        title: title,
+        // theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: const MainPage(),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  // Widget build(BuildContext context) => TabBarWidget(
+  //       title: MyApp.title,
+  //       tabs: const [
+  //         Tab(icon: Icon(Icons.sort_by_alpha), text: 'Report'),
+  //       ],
+  //       children: [
+  //         SortablePage(),
+  //       ],
+  //     );
+
+  Widget build(BuildContext context) => SortablePage();
+  // Widget build(BuildContext context) => GErrorMessage(
+  //       icon: const Icon(Icons.wifi_off_outlined),
+  //       title: "No internet",
+  //       subtitle:
+  //           "Can't connect to the internet.\nPlease check your internet connection",
+  //       onPressed: () {},
+  //     );
+}
