604e0a3d9d5e3cada5c5282fbe4d800dac2f7a6c
Richard Muragijimana <beastar457@gmail.com>
2022-02-11 13:24:33 +0200

    getting ready flipper_data_table

---
+++ packages/flipper_data_table/flipper_data_table_demo/android/app/src/main/kotlin/com/example/flipper_data_table_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flipper_data_table_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/flipper_data_table/lib/data/report_mock.dart
@@ -0,0 +1,13 @@
+import 'package:flipper_models/models/models.dart';
+
+final reports = <OrderItemSync>[
+  OrderItemSync(
+    name: "mango",
+    fvariantId: 1,
+    count: 10,
+    price: 200,
+    forderId: 1,
+    createdAt: DateTime.now().toIso8601String(),
+    updatedAt: DateTime.now().toIso8601String(),
+  )
+];
