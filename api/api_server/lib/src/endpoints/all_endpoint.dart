9e5b4ebf843413a004bef436dd4347841560b0f4
root <root@yegobox.com>
2022-03-15 07:20:09 +0000

    renaming files

--- api/api_server/lib/src/endpoints/all_endpoint.dart
+++ api/api_server/lib/src/endpoints/all_endpoint.dart
@@ -7,7 +7,7 @@ import 'package:serverpod/serverpod.dart';
 
 // After adding or modifying an endpoint, you will need to run
 // `serverpod generate` to update the server and client code.
-class ExampleEndpoint extends Endpoint {
+class AllEndpoint extends Endpoint {
   // You create methods in your endpoint which are accessible from the client by
   // creating a public method with `Session` as its first parameter. Supported
   // parameter types are `bool`, `int`, `double`, `String`, `DateTime`, and any
@@ -15,8 +15,7 @@ class ExampleEndpoint extends Endpoint {
   // should return a typed future; the same types as for the parameters are
   // supported. The `session` object provides access to the database, logging,
   // passwords, and information about the request being made to the server.
-  // Session session, String name
-  Future<String> hello() async {
-    return 'Hello';
+  Future<String> businesses(Session session, String name) async {
+    return 'Hello $name';
   }
 }
