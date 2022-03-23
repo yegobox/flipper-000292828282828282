71471ca82c0dd0999ba7862b7ea7d9f7ba13f9d7
Richard Muragijimana <beastar457@gmail.com>
2022-03-23 05:41:37 +0200

    before crazy

--- packages/flipper_login/lib/login.dart
+++ packages/flipper_login/lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flipper_login/config.dart';
+import 'config.dart';
 import 'package:flipper_rw/gate.dart';
 import 'package:flipper_services/app_service.dart';
 import 'package:flutter/material.dart';
