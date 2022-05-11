989cc0fb955ea9bad9415a6ffb1bec75749edc81
Richard <beastar457@gmail.com>
2022-05-11 13:30:30 +0200

    clean the code for testability

--- lib/web_init.dart
+++ lib/web_init.dart
@@ -1,8 +1,36 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flipper_models/isar_api.dart';
+import 'package:flipper_models/isar_models.dart';
 
 Future<void> initDb() async {
-  await IsarAPI.getDir(dbName: 'db');
+  late Isar isar;
+  isar = await Isar.open(
+    directory: 'db',
+    schemas: [
+      OrderSchema,
+      BusinessSchema,
+      BranchSchema,
+      OrderItemSchema,
+      ProductSchema,
+      VariantSchema,
+      ProfileSchema,
+      SubscriptionSchema,
+      PointsSchema,
+      StockSchema,
+      FeatureSchema,
+      VoucherSchema,
+      PColorSchema,
+      CategorySchema,
+      UnitSchema,
+      SettingSchema,
+      DiscountSyncSchema,
+      CustomerSchema,
+      PinSchema,
+      ReceiptSchema,
+    ],
+    inspector: false,
+  );
+  IsarAPI(isar: isar);
   await Firebase.initializeApp();
 }
 
