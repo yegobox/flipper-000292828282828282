ce380a374be3dcb805fa383bf3090d6c9972cb45
Richard <beastar457@gmail.com>
2022-05-19 00:56:30 +0200

    fix broken testing... ready to new steroid
    Former-commit-id: cbf425f2e89f1864d6b6205628bb52ab0bc72dc9

--- test/view_models/api_test.dart
+++ test/view_models/api_test.dart
@@ -2,6 +2,7 @@ import 'package:flipper_models/isar_api.dart';
 import 'package:flipper_models/isar_models.dart';
 import 'package:test/test.dart';
 
+import '../helpers/test_helpers.dart';
 import 'common.dart';
 
 void main() {
@@ -9,7 +10,7 @@ void main() {
     late Isar isar;
 
     setUp(() async {
-      // registerServices();
+      registerServices();
       // ensure there is a temp product
       // ensure we have custom Amount product
       // ensure we have business created and it's branch
@@ -17,11 +18,11 @@ void main() {
     });
 
     tearDown(() async {
-      // unregisterServices();
+      unregisterServices();
       await isar.close();
     });
 
-    isarTest('.Test we have business named yegobox.', () async {
+    isarTest('Test we have a temp product', () async {
       IsarAPI.instance(isarRef: isar);
       BusinessHomeViewModel viewModel = BusinessHomeViewModel();
 
