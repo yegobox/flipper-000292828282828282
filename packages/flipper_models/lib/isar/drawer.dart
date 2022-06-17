fa93966c5b771fdc5dc057fc01a0d39ae8635a5f
Muragijimana <beastar457@gmail.com>
2022-06-17 16:47:08 +0200

    145 z report according to rra spcecs (#146)
    * should not mock the database
    
    * unit testing now making sense
    
    * add toggle for more detailed transactions with rra z report
    
    * about to work on drawer
    
    * drawer working in draft mode
    
    * update the document
    
    * wrap-up the drawer feature
    
    * z-report design
    
    * x-report now  testable
    
    * now z-report is implemented.
    
    Former-commit-id: d590f2f4eac32b6166848db26a7a683392340f57

--- packages/flipper_models/lib/isar/drawer.dart
+++ packages/flipper_models/lib/isar/drawer.dart
@@ -9,7 +9,17 @@ class Drawers {
   late double closingBalance;
   String? openingDateTime;
   String? closingDateTime;
-
+  int? csSaleCount;
+  String? tradeName;
+  double? totalNsSaleIncome;
+  double? totalCsSaleIncome;
+  int? nrSaleCount;
+  int? nsSaleCount;
+  int? trSaleCount;
+  int? psSaleCount;
+  int? incompleteSale;
+  int? otherTransactions;
+  String? paymentMode;
   @Index()
   late int cashierId;
   @Index(composite: [CompositeIndex('cashierId')])
