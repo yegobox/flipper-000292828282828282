246c125cc4cc472eb60d2fc19ae2afd89a428958
Richard <beastar457@gmail.com>
2022-07-30 02:10:11 +0200

    notification system

--- packages/flipper_models/lib/marketing.dart
+++ packages/flipper_models/lib/marketing.dart
@@ -7,7 +7,10 @@ import 'package:http/http.dart' as http;
 import 'isar_models.dart';
 import 'whatsapp.dart';
 
+// https://developers.facebook.com/docs/whatsapp/on-premises/reference/messages
+
 class Marketing implements WhatsApp, SMS {
+  String wUrl = "https://graph.facebook.com/v13.0";
   @override
   Future<int> sendSMS(
       {required String message, required List<String> numbers}) {
@@ -23,14 +26,46 @@ class Marketing implements WhatsApp, SMS {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ${token!.token}'
     };
-    // https://developers.facebook.com/docs/whatsapp/on-premises/reference/messages
-    var request = http.Request('POST',
-        Uri.parse('https://graph.facebook.com/v13.0/104559025664071/messages'));
+
+    var request =
+        http.Request('POST', Uri.parse('$wUrl/104559025664071/messages'));
     request.body = json.encode({
       "messaging_product": "whatsapp",
-      "to": "250783054874",
+      "to": numbers.first,
       "type": "text",
       "text": {"body": "$message"}
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      log(await response.stream.bytesToString());
+      return 200;
+    } else {
+      return response.statusCode;
+    }
+  }
+
+  @override
+  Future optIn() async {
+    String phoneNumber = ProxyService.box.getUserPhone()!;
+    Token? token = await ProxyService.isarApi.whatsAppToken();
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${token!.token}'
+    };
+
+    var request =
+        http.Request('POST', Uri.parse('$wUrl/104559025664071/messages'));
+    request.body = json.encode({
+      "messaging_product": "whatsapp",
+      "to": phoneNumber,
+      "type": "template",
+      "template": {
+        "name": "introduction",
+        "language": {"code": "en_US"}
+      }
     });
     request.headers.addAll(headers);
 
