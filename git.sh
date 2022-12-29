d9fc7a46b23c6e828cbec38d91c4a76284fe8fd3
Richard Muragijimana <beastar457@gmail.com>
2022-12-29 15:09:59 +0200

    update git.sh to have all packages listed in open-source folder
    Former-commit-id: 52b16adb8c07b68b8d48c99eb42f55cf03ee7b33

--- git.sh
+++ git.sh
@@ -11,6 +11,14 @@ cd .. && cd win32 &&  git checkout main && git commit -am "clean-up" && git push
 cd .. && cd dart_pdf &&  git checkout main && git commit -am "clean-up" && git push origin main
 cd .. && cd flutter_chat_ui &&  git checkout main && git commit -am "clean-up" && git push origin main
 cd .. && cd flutter_firebase_chat_core &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_list_drag_and_drop &&  git checkout main && git commit -am "clean-up" && git push origin main
 cd .. && cd flutterfire_desktop &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd receipt &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd money_formatter &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd kds &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd qr_code_scanner &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd pubnub &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd upgrader &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd system_time_check &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_file_picker &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_chat_types &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd animated_toggle_switch &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd animated_toggle_switch &&  git checkout main && git commit -am "clean-up" && git push origin main
