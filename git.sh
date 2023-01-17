631ee8e579232a661c7b00e7d23fe0d58afdc412
Richard Muragijimana <beastar457@gmail.com>
2023-01-17 12:57:40 +0200

    fix some .sh error
    Former-commit-id: a1fa0721c482e72135e3c16c94e75ceddc708ea9

--- git.sh
+++ git.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-cd ./open-sources && cd cli_dialog &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd dart_console &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter.widgets &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_list_drag_and_drop &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd msix &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd ./open-sources && cd cli_dialog &&  git checkout main  && git add .   &&  git commit -am "clean-up" && git push origin main
+cd .. && cd dart_console &&  git checkout main  && git add .   && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter.widgets &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_list_drag_and_drop &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd msix &&  git checkout main && git add . &&  git commit -am "clean-up" && git push origin main
 # cd .. && cd open_file &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd qr.flutter &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd receipt &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd win32 &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd dart_pdf &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_chat_ui &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_firebase_chat_core &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutterfire_desktop &&  git commit -am "clean-up"  && git checkout main && git commit -am "clean-up"  && git push origin main
-cd .. && cd money_formatter &&  git checkout dev &&  git commit -am "clean-up"  && git push origin dev
-cd .. && cd kds &&  git checkout master && git commit -am "clean-up" && git push origin master
-cd .. && cd qr_code_scanner &&  git checkout master && git commit -am "clean-up" && git push origin master
-cd .. && cd pubnub &&  git checkout master && git commit -am "clean-up" && git push origin master
-cd .. && cd upgrader &&  git checkout master && git commit -am "clean-up" && git push origin master
-cd .. && cd system_time_check &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_file_picker &&  git checkout master && git commit -am "clean-up" && git push origin master
-cd .. && cd flutter_chat_types &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd animated_toggle_switch &&  git checkout main && git commit -am "clean-up" && git push origin main
-cd .. && cd animated_toggle_switch &&  git checkout main && git commit -am "clean-up" && git push origin main
+cd .. && cd qr.flutter &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd receipt &&  git checkout main  && git add .   && git commit -am "clean-up" && git push origin main
+cd .. && cd win32 &&  git checkout main  && git add .   && git commit -am "clean-up" && git push origin main
+cd .. && cd dart_pdf &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_chat_ui &&  git checkout main  && git add .   && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_firebase_chat_core &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd flutterfire_desktop   && git checkout main  && git add . && git add . &&  git commit -am "clean-up"  && git push origin main
+cd .. && cd money_formatter &&  git checkout dev  && git add .   &&  git commit -am "clean-up"  && git push origin dev
+cd .. && cd kds &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+cd .. && cd qr_code_scanner &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+cd .. && cd pubnub &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+cd .. && cd system_time_check &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_file_picker &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+cd .. && cd flutter_chat_types &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+cd .. && cd animated_toggle_switch &&  git checkout  && git add . &&  main && git commit -am "clean-up" && git push origin main
+cd .. && cd animated_toggle_switch &&  git checkout  && git add . &&  main && git commit -am "clean-up" && git push origin main
