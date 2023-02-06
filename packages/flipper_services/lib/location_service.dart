d1302b206df158016b6797469b8526e3231c3eb8
Richard Muragijimana <beastar457@gmail.com>
2023-02-06 11:48:07 +0200

    fixing
    Former-commit-id: aac8e11c82aa800907ed556fc41eedcca7911bab

--- packages/flipper_services/lib/location_service.dart
+++ packages/flipper_services/lib/location_service.dart
@@ -7,10 +7,6 @@ import 'package:location/location.dart';
 final defaultlocation = {"longitude": "11", "latitude": "11"};
 
 class LocationService implements FlipperLocation {
-  late PermissionStatus _serviceEnabled;
-  late PermissionStatus _permissionGranted;
-
-  @override
   Future<Map<String, String>> getLocations() async {
     try {
       final location = await getLocation();
@@ -19,26 +15,13 @@ class LocationService implements FlipperLocation {
         'latitude': location.latitude.toString()
       };
     } catch (e) {
-      // ignore: todo
-      // TODO:keep checking as soon windows is supported to change this
       return {'longitude': "1.1", 'latitude': "1.1"};
     }
   }
 
-  @override
   Future<bool> doWeHaveLocationPermission() async {
-    _serviceEnabled = await getPermissionStatus();
-    if (!_serviceEnabled.authorized) {
-      return false;
-    }
-    _permissionGranted = await requestPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await requestPermission();
-      if ((_permissionGranted != PermissionStatus.authorizedAlways) ||
-          (_permissionGranted != PermissionStatus.authorizedWhenInUse)) {
-        return false;
-      }
-    }
-    return true;
+    final permissionStatus = await requestPermission();
+    return permissionStatus == PermissionStatus.authorizedAlways ||
+        permissionStatus == PermissionStatus.authorizedWhenInUse;
   }
 }
