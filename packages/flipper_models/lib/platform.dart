09abdda6a4ed0f879d69c3722caada67fdd7cbdf
Richard Muragijimana <beastar457@gmail.com>
2023-02-06 13:14:35 +0200

    fixing#9
    Former-commit-id: f89acb6f53bed0ea7c53a6ca850843b9c10cd801

--- packages/flipper_models/lib/platform.dart
+++ packages/flipper_models/lib/platform.dart
@@ -1,15 +1,10 @@
-import 'dart:io';
-
 String? url;
 String? password;
 
 void getEnvVariables() {
-  if (Platform.environment.containsKey('DB_URL')) {
-    url = Platform.environment['DB_URL'];
-  }
-  if (Platform.environment.containsKey('DB_PASSWORD')) {
-    password = Platform.environment['DB_PASSWORD'];
-  }
+  url = String.fromEnvironment('DB_URL');
+
+  password = String.fromEnvironment('DB_PASSWORD');
 }
 
 // For testing purpose@NOTE: please add this in your operating system environment variable for testing.
