aac8e11c82aa800907ed556fc41eedcca7911bab
Richard Muragijimana <beastar457@gmail.com>
2023-02-06 11:48:07 +0200

    fixing

--- packages/flipper_models/lib/remote_service.dart
+++ packages/flipper_models/lib/remote_service.dart
@@ -1,10 +1,15 @@
-import 'dart:developer';
-
-import 'package:flipper_models/platform.dart';
-import 'package:pocketbase/pocketbase.dart';
+// abstract class RemoteInterface<T> {
+//   Future<List<RecordModel>> getCollection({required String collectionName});
+//   Future<void> create(
+//       {required Map<String, dynamic> collection,
+//       required String collectionName});
+//   Future<void> update(
+//       {required Map<String, dynamic> updateCollection,
+//       required String collectionName});
+// }
 
 abstract class RemoteInterface<T> {
-  Future<List<RecordModel>> getCollection({required String collectionName});
+  Future<List<dynamic>> getCollection({required String collectionName});
   Future<void> create(
       {required Map<String, dynamic> collection,
       required String collectionName});
@@ -14,48 +19,73 @@ abstract class RemoteInterface<T> {
 }
 
 class RemoteService<T> implements RemoteInterface {
-  late PocketBase pb;
   Future<RemoteInterface> getInstance() async {
-    pb = PocketBase(url!);
-    await pb.admins.authWithPassword('info@yegobox.com', password!);
     return this;
-  }
-
-  void putInSync() {
-    pb
-        .collection('orders')
-        .subscribe("*", (e) {})
-        .catchError((error, stackTrace) {
-      log(error);
-      log(stackTrace);
-    });
-  }
-
-  @override
-  Future<List<RecordModel>> getCollection({required String collectionName}) {
-    return pb.collection(collectionName).getFullList();
   }
 
   @override
   Future<void> create(
       {required Map<String, dynamic> collection,
-      required String collectionName}) async {
-    pb.collection(collectionName).create(body: collection).then((record) {
-      print(record.id);
-      print(record.getStringValue('id'));
-    });
+      required String collectionName}) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List> getCollection({required String collectionName}) {
+    // TODO: implement getCollection
+    throw UnimplementedError();
   }
 
   @override
   Future<void> update(
       {required Map<String, dynamic> updateCollection,
       required String collectionName}) {
-    return pb
-        .collection(collectionName)
-        .update(updateCollection["id"], body: updateCollection)
-        .then((record) {
-      print(record.id);
-      print(record.getStringValue('id'));
-    });
+    // TODO: implement update
+    throw UnimplementedError();
   }
+  // late PocketBase pb;
+  // Future<RemoteInterface> getInstance() async {
+  //   pb = PocketBase(url!);
+  //   await pb.admins.authWithPassword('info@yegobox.com', password!);
+  //   return this;
+  // }
+
+  // void putInSync() {
+  //   pb
+  //       .collection('orders')
+  //       .subscribe("*", (e) {})
+  //       .catchError((error, stackTrace) {
+  //     log(error);
+  //     log(stackTrace);
+  //   });
+  // }
+
+  // @override
+  // Future<List<RecordModel>> getCollection({required String collectionName}) {
+  //   return pb.collection(collectionName).getFullList();
+  // }
+
+  // @override
+  // Future<void> create(
+  //     {required Map<String, dynamic> collection,
+  //     required String collectionName}) async {
+  //   pb.collection(collectionName).create(body: collection).then((record) {
+  //     print(record.id);
+  //     print(record.getStringValue('id'));
+  //   });
+  // }
+
+  // @override
+  // Future<void> update(
+  //     {required Map<String, dynamic> updateCollection,
+  //     required String collectionName}) {
+  //   return pb
+  //       .collection(collectionName)
+  //       .update(updateCollection["id"], body: updateCollection)
+  //       .then((record) {
+  //     print(record.id);
+  //     print(record.getStringValue('id'));
+  //   });
+  // }
 }
