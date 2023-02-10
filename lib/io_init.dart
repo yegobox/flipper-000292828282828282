e2865d7bf30331d1df18450556c651d127851daa
Richard Muragijimana <beastar457@gmail.com>
2023-02-10 16:12:44 +0200

    test integration testing
    Former-commit-id: e8f684d64417811128a334058e0845d095b167fa

--- lib/io_init.dart
+++ lib/io_init.dart
@@ -3,10 +3,8 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flipper_models/isar_models.dart';
 import 'package:flutter/foundation.dart';
-import 'package:universal_platform/universal_platform.dart';
 import 'firebase_options.dart';
 
-final isAndroid = UniversalPlatform.isAndroid;
 Future<void> initDb() async {
   // await ObjectBoxApi.getDir(dbName: 'db');
   if (Platform.isIOS || Platform.isMacOS || Platform.isAndroid) {
