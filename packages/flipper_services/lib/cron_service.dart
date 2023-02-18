61868ce7dbda272de916614c5d9189201dfde2b5
Richard Muragijimana <beastar457@gmail.com>
2023-02-18 21:42:48 +0200

    add notifiction in status bar when there is no internet for a user to be aware we are working in offline mode

--- packages/flipper_services/lib/cron_service.dart
+++ packages/flipper_services/lib/cron_service.dart
@@ -1,10 +1,6 @@
 import 'dart:async';
 import 'dart:io';
-
-import 'package:cron/cron.dart';
-// import 'package:flipper_models/models/models.dart' as box;
 import 'package:flipper_models/isar_models.dart';
-// import 'package:flipper_models/order_item.dart';
 import 'package:flipper_services/abstractions/printer.dart';
 import 'package:flipper_services/constants.dart';
 import 'package:flipper_services/drive_service.dart';
@@ -16,44 +12,20 @@ import 'package:flipper_services/setting_service.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 class CronService {
-  final cron = Cron();
   final settingService = locator<SettingsService>();
   final printer = locator<Printer>();
   final log = getLogger('CronService');
   final drive = GoogleDrive();
 
-  /// This is the report mainly for yegobox|flipper business
-  /// the report shall help the company know what customers are selling
-  /// when they are selling the item and to him plus any other relevant information
-  /// that can help sales team to develop product and increase sales
   void companyWideReport() {}
-
-  /// this report should be sent to a customer given email.
-  /// for a daly report, should create a spreadsheet tab naming it with the date of the report
-  /// when a customer update the email, it should trigger the server(us) to create a new google sheet document
-  /// and add it to the setting modal of the client,then on report generation the same document should be used
-  /// so the document will be share do the user's email.
-  /// check the user has spreadsheet document assigned, a log out and log in can assist
-  /// since we update the business permissions and everytime a login happen we update business model from the server
 
   void customerBasedReport() {}
   //bluethooth should search and auto connect to any near bluethoopt printer.
   //the setting should enable this i.e toggled to true i.e we will start the search process on app startup
   //the search process should be triggered by a button on the settings page
-  connectBlueToothPrinter() {
-    //this is the code to connect to bluetooth printer
-    cron.schedule(Schedule.parse('*/1 * * * *'), () async {
-      // log.i('start connecting bluethooth');
-      // printer.connect();
-    });
-  }
+  connectBlueToothPrinter() {}
 
-  deleteReceivedMessageFromServer() {
-    // cron.schedule(Schedule.parse('*/1 * * * *'), () async {
-    //   log.i('scheduled delete message');
-    //   ProxyService.isarApi.emptyQueue();
-    // });
-  }
+  deleteReceivedMessageFromServer() {}
 
   //the default file that will be generated and saved in known app folder
   //will be printed everytime a sales is complete for demo
@@ -69,39 +41,7 @@ class CronService {
       Map updatedBusiness = business!.toJson();
       updatedBusiness['deviceToken'] = token.toString();
     }
-
-    /// load new contacts i.e business every 5 minutes
-    /// this will be used to update the business model
-    /// TODOchange this to avoid multiple api calls to the server
-    /// load them when app start then do it later every 15 minutes
-    // ProxyService.isarApi.getContacts();
-    // cron.schedule(Schedule.parse('*/45 * * * *'), () async {
-    //   ProxyService.isarApi.getContacts();
-    // });
-
-    /// backup the user db every day
-    // cron.schedule(Schedule.parse('*/60 * * * *'), () async {
-    //   log.i('downloading the remote  copy');
-    //   // for now enable backup for all clients in future this will be changed
-    //   // Business? business = await ProxyService.isarApi.getBusiness();
-    //   // prevent the backup pop-up when a user did not click on adding backup button.
-    //   if (ProxyService.box.hasSignedInForAutoBackup()) {
-    //     await drive.downloadGoogleDriveFile(
-    //         'mdbx.dat', ProxyService.box.gdID());
-
-    //     Directory test = await getApplicationDocumentsDirectory();
-
-    //     await for (var entity
-    //         in test.list(recursive: true, followLinks: false)) {
-    //       log.i(entity.path);
-    //     }
-    //   }
-    // });
-    cron.schedule(Schedule.parse('*/30 * * * *'), () async {
-      log.i('uploading the local copy');
-      // for now enable backup for all clients in future this will be changed
-      // Business? business = await ProxyService.isarApi.getBusiness();
-      // prevent the backup pop-up when a user did not click on adding backup button.
+    Timer.periodic(Duration(minutes: 30), (Timer t) async {
       if (ProxyService.box.hasSignedInForAutoBackup()) {
         drive.upload();
       }
@@ -131,12 +71,6 @@ class CronService {
             .completedOrders(branchId: ProxyService.box.getBranchId()!);
         print('how many time cron runs');
 
-        ProxyService.notification.localNotification(
-            1,
-            "Backup data",
-            "we are backing up your data",
-            tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)));
-
         for (Order completedOrder in completedOrders) {
           if (processedOrders.contains(completedOrder.id)) {
             return;
@@ -156,6 +90,11 @@ class CronService {
             completedOrder.reported = true;
 
             if (await ProxyService.appService.checkInternetConnectivity()) {
+              ProxyService.notification.localNotification(
+                  1,
+                  "Backup data",
+                  "we are backing up your data",
+                  tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)));
               try {
                 await ProxyService.isarApi.update(data: completedOrder);
                 await ProxyService.remoteApi.create(
