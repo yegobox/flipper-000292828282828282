48ff99136828d06b5cd1e5d3a88fbbe1888fb825
Richard Muragijimana <beastar457@gmail.com>
2023-02-22 09:35:41 +0200

    fix optin on whatsapp when signup

--- lib/main.dart
+++ lib/main.dart
@@ -132,6 +132,7 @@ void main() async {
         // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
         // We recommend adjusting this value in production.
         options.tracesSampleRate = 1.0;
+        options.attachScreenshot = true;
       },
     );
 
--- packages/flipper_models/lib/whatsapp.dart
+++ packages/flipper_models/lib/whatsapp.dart
@@ -1,5 +1,3 @@
 abstract class WhatsApp {
-  Future<int> sendWhatsAppMessages(
-      {required String message, required List<String> numbers});
   Future optIn();
 }
