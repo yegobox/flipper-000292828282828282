220e7398ef75be6170a4cd442271bfc9620d197e
Richard Muragijimana <beastar457@gmail.com>
2023-02-23 10:26:39 +0200

    fix potential case where we can fail to back the order just because we can't guarantee the expected server's ID
    Former-commit-id: ca8d171f120d18a7ff2670911a11228babab3d1b

--- packages/flipper_services/lib/app_service.dart
+++ packages/flipper_services/lib/app_service.dart
@@ -202,22 +202,17 @@ class AppService with ListenableServiceMixin {
     List<OrderItem> updatedItems =
         await ProxyService.isarApi.orderItems(orderId: order.id);
     order.subTotal = updatedItems.fold(0, (a, b) => a + (b.price * b.qty));
-    order.reported = true;
 
-    // try {
     /// fix@issue where the createdAt synced on server is older compared to when a transaction was completed.
     order.updatedAt = DateTime.now().toIso8601String();
     order.createdAt = DateTime.now().toIso8601String();
-    await ProxyService.isarApi.update(data: order);
+
     await ProxyService.remoteApi.create(
         collection:
             order.toJson(convertIdToString: true, itemName: namesString),
         collectionName: 'orders');
-    // } catch (e, stackTrace) {
-    //   order.reported = false;
-    //   await ProxyService.isarApi.update(data: order);
-    //   ProxyService.crash.reportError(e, stackTrace);
-    // }
+    order.reported = true;
+    await ProxyService.isarApi.update(data: order);
   }
 
 // The updated automaticBackup() method
