ba679d68d3965aa2d24b3d61de192fb23e0ae406
Richard Muragijimana <beastar457@gmail.com>
2023-03-23 16:06:15 +0200

    getting flipper socials bootstraped
    Former-commit-id: 44d35424419d394c6ee2cd0d6722ae3dbb6d11b5

---
+++ packages/flipper_socials/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flipper_socials/app/app.bottomsheets.dart';
+import 'package:flipper_socials/app/app.dialogs.dart';
+import 'package:flipper_socials/app/app.locator.dart';
+import 'package:flipper_socials/app/app.router.dart';
+import 'package:flipper_socials/ui/common/app_colors.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+void main() {
+  setupLocator();
+  setupDialogUi();
+  setupBottomSheetUi();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: Theme.of(context).copyWith(
+        primaryColor: kcBackgroundColor,
+        focusColor: kcPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+            ),
+      ),
+      initialRoute: Routes.startupView,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      navigatorKey: StackedService.navigatorKey,
+      navigatorObservers: [
+        StackedService.routeObserver,
+      ],
+    );
+  }
+}
