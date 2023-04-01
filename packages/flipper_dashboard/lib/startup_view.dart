f1085af94ecf54f8f7a5b362041995918ace1395
Richard Muragijimana <beastar457@gmail.com>
2023-04-01 12:12:12 +0200

    apply default way of showing model and bottomsheets
    Former-commit-id: 96876367ac453432951793fda57fda81f6de6d50

--- packages/flipper_dashboard/lib/startup_view.dart
+++ packages/flipper_dashboard/lib/startup_view.dart
@@ -7,59 +7,61 @@ import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
 import 'package:flipper_socials/ui/common/ui_helpers.dart';
 
-class StartUpView extends StatelessWidget {
+class StartUpView extends StackedView<StartupViewModel> {
   const StartUpView({Key? key, this.invokeLogin}) : super(key: key);
   final bool? invokeLogin;
   @override
-  Widget build(BuildContext context) {
-    return ViewModelBuilder<StartupViewModel>.reactive(
-      fireOnViewModelReadyOnce: true,
-      onViewModelReady: (model) =>
-          SchedulerBinding.instance.addPostFrameCallback(
-        (timeStamp) async {
-          await Future.delayed(Duration(seconds: 2));
-          model.runStartupLogic(invokeLogin: invokeLogin ?? false);
-        },
-      ),
-      viewModelBuilder: () => StartupViewModel(context: context),
-      builder: (context, model, child) {
-        return Scaffold(
-          body: Center(
-            child: Column(
+  Widget builder(
+    BuildContext context,
+    StartupViewModel viewModel,
+    Widget? child,
+  ) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Flipper',
+              style: GoogleFonts.poppins(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w900),
+            ),
+            Row(
               mainAxisSize: MainAxisSize.min,
-              children: [
+              children: const [
                 Text(
-                  'Flipper',
-                  style: GoogleFonts.poppins(
-                      color: Colors.black,
-                      fontSize: 40,
-                      fontWeight: FontWeight.w900),
+                  'A revolutionary business software ...',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text(
-                      'A revolutionary business software ...',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    horizontalSpaceSmall,
-                    SizedBox(
-                      width: 16,
-                      height: 16,
-                      child: CircularProgressIndicator(
-                        color: Colors.black,
-                        strokeWidth: 6,
-                      ),
-                    )
-                  ],
-                ),
+                horizontalSpaceSmall,
+                SizedBox(
+                  width: 16,
+                  height: 16,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                    strokeWidth: 6,
+                  ),
+                )
               ],
             ),
-          ),
-        );
-      },
+          ],
+        ),
+      ),
     );
   }
+
+  @override
+  StartupViewModel viewModelBuilder(
+    BuildContext context,
+  ) =>
+      StartupViewModel(context: context);
+
+  @override
+  void onViewModelReady(StartupViewModel viewModel) =>
+      SchedulerBinding.instance.addPostFrameCallback((timeStamp) =>
+          viewModel.runStartupLogic(invokeLogin: invokeLogin ?? false));
 }
