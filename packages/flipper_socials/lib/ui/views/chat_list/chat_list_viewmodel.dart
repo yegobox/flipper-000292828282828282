3c3d5401138e3c804083a2e054f9dcacdcf60eb6
Richard Muragijimana <beastar457@gmail.com>
2023-04-07 12:50:59 +0200

    experiment #7

--- packages/flipper_socials/lib/ui/views/chat_list/chat_list_viewmodel.dart
+++ packages/flipper_socials/lib/ui/views/chat_list/chat_list_viewmodel.dart
@@ -1,3 +1,10 @@
 import 'package:stacked/stacked.dart';
 
-class ChatListViewModel extends BaseViewModel {}
+class ChatListViewModel extends BaseViewModel {
+  bool _focusedConversation = false;
+  bool get focusedConversation => _focusedConversation;
+  set focusedConversation(bool v) {
+    _focusedConversation = true;
+    notifyListeners();
+  }
+}
