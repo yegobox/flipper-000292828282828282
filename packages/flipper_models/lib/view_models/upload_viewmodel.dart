9a9785149abfa6776dff74f02fda45cfefa63fb2
Richard Muragijimana <beastar457@gmail.com>
2023-05-12 06:33:36 +0200

    fixing #1
    Former-commit-id: f21116e2cf336c59eeba3b120f2343b7ad75bdc1

--- packages/flipper_models/lib/view_models/upload_viewmodel.dart
+++ packages/flipper_models/lib/view_models/upload_viewmodel.dart
@@ -5,11 +5,13 @@ import 'package:flipper_services/abstractions/upload.dart';
 import 'package:flipper_services/proxy.dart';
 import 'package:flipper_services/upload_response.dart';
 import 'package:flutter_uploader/flutter_uploader.dart';
+import 'package:flipper_services/locator.dart' as loc;
+import 'package:flipper_services/app_service.dart';
 
 class UploadViewModel extends ProductViewModel {
   FlutterUploader uploader = FlutterUploader();
   List<String> processed = <String>[];
-
+  final appService = loc.locator<AppService>();
   void browsePictureFromGallery(
       {required int id,
       required Function(String) callBack,
@@ -61,5 +63,25 @@ class UploadViewModel extends ProductViewModel {
       required URLTYPE urlType}) {
     ProxyService.upload
         .takePicture(urlType: urlType, id: productId, uploader: uploader);
+  }
+
+  void updateBusinessProfile({required String url}) async {
+    ITenant? tenant = await ProxyService.isarApi
+        .getTenantBYUserId(userId: ProxyService.box.getUserId()!);
+    if (tenant != null) {
+      tenant.imageUrl = url;
+      ProxyService.isarApi.update(data: tenant);
+    }
+
+    /// if the user has enabled the flipper connecta update his profile image in contacts as well
+    if (await appService.isSocialLoggedin()) {
+      // we are logged in in social so safe to patch the image as well
+      ProxyService.isarApi.updateContact(contact: {
+        "phoneNumber": ProxyService.box.getUserPhone(),
+        "avatar": url,
+        "entity": "contacts",
+        "businessId": ProxyService.box.getBusinessId
+      }, businessId: ProxyService.box.getBusinessId()!);
+    }
   }
 }
