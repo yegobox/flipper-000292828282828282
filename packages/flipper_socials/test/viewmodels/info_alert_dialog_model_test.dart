9e5458d25ff371913512148c9357c2b12caa3404
Richard Muragijimana <beastar457@gmail.com>
2023-05-12 15:04:22 +0200

    experiment flutter 3.10
    Former-commit-id: b09846301ace1a2b148dc755cd1222edc2da7481

--- packages/flipper_socials/test/viewmodels/info_alert_dialog_model_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import '../helpers/test_helpers.dart';
-
-void main() {
-  group('InfoAlertDialogModel Tests -', () {
-    setUp(() => registerServices());
-  });
-}
