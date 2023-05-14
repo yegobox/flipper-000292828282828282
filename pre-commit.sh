6ab61dc53979ece63cafcb557c8dc1b9088f9ef2
Richard Muragijimana <beastar457@gmail.com>
2023-05-14 06:33:17 +0200

    pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# pre-commit.sh
 
-# get the current version number from pubspec.yaml
-version=$(grep 'version' pubspec.yaml | awk '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+# get current version
+version=$(grep "version: " pubspec.yaml | cut -d' ' -f2)
 
-# increment the version number
-new_version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+# increment version
+major=$(echo $version | cut -d'.' -f1)
+minor=$(echo $version | cut -d'.' -f2)
+patch=$(echo $version | cut -d'.' -f3)
+patch=$((patch+1))
+new_version="$major.$minor.$patch"
 
-# replace the old version number with the new one in pubspec.yaml
-sed -i "s/version: $version/version: $new_version/g" pubspec.yaml
-
-# add the modified pubspec.yaml file to the commit
-git add pubspec.yaml
+# replace version in pubspec.yaml
+sed -i "s/version: $version/version: $new_version/" pubspec.yaml
