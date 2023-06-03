3c09950ac92fa0af02c08edf0ccf8ada17266f00
Richard Muragijimana <beastar457@gmail.com>
2023-06-03 12:26:35 +0200

    ...

--- packages/flipper_services/lib/cron_service.dart
+++ packages/flipper_services/lib/cron_service.dart
@@ -52,6 +52,7 @@ class CronService {
     Timer.periodic(Duration(seconds: kDebugMode ? 10 : 10), (Timer t) async {
       /// get unsynced counter and send them online for houseKeping.
       ProxyService.isar.sendScheduleMessages();
+      ProxyService.event.keepTryingPublishDevice();
     });
     Timer.periodic(Duration(minutes: kDebugMode ? 1 : 10), (Timer t) async {
       /// get unsynced counter and send them online for houseKeping.
