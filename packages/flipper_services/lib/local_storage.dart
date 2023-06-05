f3df1d8c9e9540605760e895e4da75df51103c5d
Richard Muragijimana <beastar457@gmail.com>
2023-06-05 11:53:24 +0200

    test shared preference version of the box
    Former-commit-id: 65351b7b1c03e9e6125d78e80fe115a8f2ebc846

--- packages/flipper_services/lib/local_storage.dart
+++ packages/flipper_services/lib/local_storage.dart
@@ -1,5 +1,151 @@
 import 'abstractions/storage.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferenceStorage implements LocalStorage {
+  late SharedPreferences prefs;
+  Future<LocalStorage> initializePreferences() async {
+    prefs = await SharedPreferences.getInstance();
+    return this;
+  }
+
+  @override
+  dynamic read({required String key}) {
+    return prefs.get(key);
+  }
+
+  @override
+  Future<bool> write({required dynamic key, required dynamic value}) async {
+    if (value is String) {
+      return prefs.setString(key, value);
+    } else if (value is int) {
+      return prefs.setInt(key, value);
+    } else if (value is double) {
+      return prefs.setDouble(key, value);
+    } else if (value is bool) {
+      return prefs.setBool(key, value);
+    }
+    return false;
+  }
+
+  @override
+  void remove({required String key}) {
+    prefs.remove(key);
+  }
+
+  @override
+  int? getBranchId() {
+    return prefs.getInt('branchId');
+  }
+
+  @override
+  int? getBusinessId() {
+    return prefs.getInt('businessId');
+  }
+
+  @override
+  int? getUserId() {
+    final userId = prefs.get('userId');
+    if (userId is String && isNumeric(userId)) {
+      return int.parse(userId);
+    } else if (userId is int) {
+      return userId;
+    }
+    return null;
+  }
+
+  bool isNumeric(String? s) {
+    if (s == null) {
+      return false;
+    }
+    return int.tryParse(s) != null;
+  }
+
+  @override
+  String? getUserPhone() {
+    return prefs.getString('userPhone');
+  }
+
+  @override
+  bool getNeedAccountLinkWithPhone() {
+    return prefs.getBool('needLinkPhoneNumber') ?? false;
+  }
+
+  @override
+  String? getServerUrl() {
+    return prefs.getString('serverUrl');
+  }
+
+  @override
+  int? currentOrderId() {
+    return prefs.getInt('currentOrderId');
+  }
+
+  @override
+  bool isPoroformaMode() {
+    return prefs.getBool('isPoroformaMode') ?? false;
+  }
+
+  @override
+  bool isTrainingMode() {
+    return prefs.getBool('isTrainingMode') ?? false;
+  }
+
+  @override
+  bool isAutoPrintEnabled() {
+    return prefs.getBool('isAutoPrintEnabled') ?? false;
+  }
+
+  @override
+  bool isAutoBackupEnabled() {
+    return prefs.getBool('isAutoBackupEnabled') ?? false;
+  }
+
+  @override
+  bool hasSignedInForAutoBackup() {
+    return prefs.getBool('hasSignedInForAutoBackup') ?? false;
+  }
+
+  @override
+  String? gdID() {
+    return prefs.getString('gdID');
+  }
+
+  @override
+  bool isAnonymous() {
+    return prefs.getBool('isAnonymous') ?? false;
+  }
+
+  @override
+  String? getBearerToken() {
+    return prefs.getString('bearerToken');
+  }
+
+  @override
+  bool? getIsTokenRegistered() {
+    return prefs.getBool('getIsTokenRegistered');
+  }
+
+  @override
+  int getDefaultApp() {
+    return prefs.getInt('defaultApp') ?? 1;
+  }
+
+  @override
+  String? getSocialBearerToken() {
+    return prefs.getString('socialBearerToken');
+  }
+
+  @override
+  String? getPk() {
+    return prefs.getString('pk') ?? '';
+  }
+
+  @override
+  String? getSk() {
+    return prefs.getString('sk') ?? '';
+  }
+}
 
 class LocalStorageImpl implements LocalStorage {
   final box = GetStorage();
