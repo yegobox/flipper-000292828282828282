2ed9fc9e701727bcd7679296d9993cb3891be5ff
Muragijimana <info@yegobox.com>
2023-08-09 05:00:21 +0200

    Isar4 (#212)
    * rename stable to dev
    
    * ability to sync TransactionItem
    
    * more improvements.. and code harimonizer
    
    * annotate transactionItem action
    
    * fix github actions
    
    * make action required field and fix overflow on country picking screen
    
    * make action required field and fix overflow on country picking screen
    
    * remove some fields that was no need to use them.. more cleaning
    
    * fixing...
    
    * fixing...
    
    * fix action that might miss when on data fetched
    
    * fix null
    
    * use DateTime on lastTouched to simplify the comparizon,syncing
    
    * fix possible error when sending local changes
    
    * fix spotted bug when null value was accessed on empty stock
    
    * report the bug to sentry
    
    * more stability fix
    
    * more stability fix
    
    * major release with isar v4 and web fix
    
    * submodules..update
    
    * fix pull request action
    
    * fix pull request action
    
    * fix ci
    
    * fix ci
    
    * fix min sdk

---
+++ lib/const/resource.dart
@@ -0,0 +1,118 @@
+/// Generate by [flutter_assets_generator](https://github.com/goodswifter/flutter_assets_generator) library.
+///
+/// PLEASE DO NOT EDIT MANUALLY.
+
+class R {
+  const R._();
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\checkout.svg)
+  static const String assetsCheckoutSvg = 'assets/checkout.svg';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\flipper_logo.png)
+  static const String assetsFlipperLogoPng = 'assets/flipper_logo.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\icon.png)
+  static const String assetsIconPng = 'assets/icon.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\image_2.png)
+  static const String assetsImage2Png = 'assets/image_2.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\image_3.png)
+  static const String assetsImage3Png = 'assets/image_3.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\image_4.png)
+  static const String assetsImage4Png = 'assets/image_4.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\logo.png)
+  static const String assetsLogoPng = 'assets/logo.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\logo.svg)
+  static const String assetsLogoSvg = 'assets/logo.svg';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\main.png)
+  static const String assetsMainPng = 'assets/main.png';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\more.svg)
+  static const String assetsMoreSvg = 'assets/more.svg';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\notifications.svg)
+  static const String assetsNotificationsSvg = 'assets/notifications.svg';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\assets\transactions.svg)
+  static const String assetsTransactionsSvg = 'assets/transactions.svg';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\OFL.txt)
+  static const String googleFontsOFLTxt = 'google_fonts/OFL.txt';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Black.ttf)
+  static const String googleFontsPoppinsBlackTtf =
+      'google_fonts/Poppins-Black.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-BlackItalic.ttf)
+  static const String googleFontsPoppinsBlackItalicTtf =
+      'google_fonts/Poppins-BlackItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Bold.ttf)
+  static const String googleFontsPoppinsBoldTtf =
+      'google_fonts/Poppins-Bold.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-BoldItalic.ttf)
+  static const String googleFontsPoppinsBoldItalicTtf =
+      'google_fonts/Poppins-BoldItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-ExtraBold.ttf)
+  static const String googleFontsPoppinsExtraBoldTtf =
+      'google_fonts/Poppins-ExtraBold.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-ExtraBoldItalic.ttf)
+  static const String googleFontsPoppinsExtraBoldItalicTtf =
+      'google_fonts/Poppins-ExtraBoldItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-ExtraLight.ttf)
+  static const String googleFontsPoppinsExtraLightTtf =
+      'google_fonts/Poppins-ExtraLight.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-ExtraLightItalic.ttf)
+  static const String googleFontsPoppinsExtraLightItalicTtf =
+      'google_fonts/Poppins-ExtraLightItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Italic.ttf)
+  static const String googleFontsPoppinsItalicTtf =
+      'google_fonts/Poppins-Italic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Light.ttf)
+  static const String googleFontsPoppinsLightTtf =
+      'google_fonts/Poppins-Light.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-LightItalic.ttf)
+  static const String googleFontsPoppinsLightItalicTtf =
+      'google_fonts/Poppins-LightItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Medium.ttf)
+  static const String googleFontsPoppinsMediumTtf =
+      'google_fonts/Poppins-Medium.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-MediumItalic.ttf)
+  static const String googleFontsPoppinsMediumItalicTtf =
+      'google_fonts/Poppins-MediumItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Regular.ttf)
+  static const String googleFontsPoppinsRegularTtf =
+      'google_fonts/Poppins-Regular.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-SemiBold.ttf)
+  static const String googleFontsPoppinsSemiBoldTtf =
+      'google_fonts/Poppins-SemiBold.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-SemiBoldItalic.ttf)
+  static const String googleFontsPoppinsSemiBoldItalicTtf =
+      'google_fonts/Poppins-SemiBoldItalic.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-Thin.ttf)
+  static const String googleFontsPoppinsThinTtf =
+      'google_fonts/Poppins-Thin.ttf';
+
+  /// ![preview](file://C:\Users\SPENN\Documents\dev-fli\flipper\google_fonts\Poppins-ThinItalic.ttf)
+  static const String googleFontsPoppinsThinItalicTtf =
+      'google_fonts/Poppins-ThinItalic.ttf';
+}
