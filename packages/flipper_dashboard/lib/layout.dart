dbf22392f22da75a5fed07a43c74571c7d0c315f
Richard Muragijimana <beastar457@gmail.com>
2023-08-30 15:00:23 +0200

    getting ready to work on purchases

--- packages/flipper_dashboard/lib/layout.dart
+++ packages/flipper_dashboard/lib/layout.dart
@@ -1,93 +1,51 @@
-import 'package:flipper_dashboard/page_switcher.dart';
 import 'package:flipper_dashboard/product_view.dart';
+import 'package:flipper_dashboard/apps.dart';
+import 'package:flipper_dashboard/checkout.dart';
 import 'package:flipper_models/isar_models.dart';
 import 'package:flutter/material.dart';
 
-class AppLayoutDraw extends StatelessWidget {
-  const AppLayoutDraw({
-    super.key,
+class AppLayoutDrawer extends StatelessWidget {
+  const AppLayoutDrawer({
+    Key? key,
     required this.controller,
-    required this.tabselected,
+    required this.tabSelected,
     required this.model,
-  });
+  }) : super(key: key);
 
   final TextEditingController controller;
-  final int tabselected;
+  final int tabSelected;
   final HomeViewModel model;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      if (constraints.maxWidth < 600) {
-        // this is a phone
-
-        return PageSwitcher(
-          controller: controller,
-          model: model,
-          currentPage: tabselected,
-        );
-      } else {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: 20,
-              child: SizedBox.shrink(),
-            ),
-            // Left menu
-            // ignore: todo
-            // TODO: left menu will be essential when we add socials feature
-            // by the time I implement it I will remove the above SizedBox as it is helping me
-            // to keep the design consistent for now.
-            // Container(
-            //   width: 150,
-            //   child: SideMenu(
-            //     style: SideMenuStyle(
-            //       showTooltip: false,
-            //       displayMode: SideMenuDisplayMode.compact,
-            //       compactSideMenuWidth: 60,
-            //       openSideMenuWidth: 150,
-            //     ),
-            //     items: [
-            //       SideMenuItem(
-            //         // Priority of item to show on SideMenu, lower value is displayed at the top
-            //         priority: 0,
-            //         title: 'Dashboard',
-            //         icon: Icon(Icons.home),
-            //         badgeContent: Text(
-            //           '3',
-            //           style: TextStyle(color: Colors.white),
-            //         ),
-            //       )
-            //     ],
-            //     controller: sideMenu,
-            //   ),
-            // ),
-
-            // Middle menu
-            Expanded(
-              flex: 2,
-              child: Container(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth < 600) {
+          return Apps(
+            isBigScreen: false,
+            controller: controller,
+            model: model,
+          );
+        } else {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 20,
+                child: const SizedBox.shrink(),
+              ),
+              Expanded(
+                flex: 2,
                 child: ProductView.normalMode(),
               ),
-            ),
-
-            // Right menu
-            Expanded(
-              flex: 1,
-              child: Container(
-                child: PageSwitcher(
-                  isBigScreen: true,
-                  controller: controller,
-                  model: model,
-                  currentPage: tabselected,
-                ),
+              Expanded(
+                flex: 1,
+                child: CheckOut(isBigScreen: true),
               ),
-            ),
-          ],
-        );
-      }
-    });
+            ],
+          );
+        }
+      },
+    );
   }
 }
