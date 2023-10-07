e28d69534e1e1315ef3d0df851e3adb04ce50192
Richard Muragijimana <beastar457@gmail.com>
2023-10-07 05:00:01 +0200

    google drive service

--- packages/flipper_services/lib/GoogleDrive.dart
+++ packages/flipper_services/lib/GoogleDrive.dart
@@ -1,22 +1,16 @@
 //TODOkeep track of the original code: https://stackoverflow.com/questions/65784077/how-do-i-upload-a-file-to-google-drive-using-flutter
 import 'dart:convert';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flipper_services/proxy.dart';
-// import 'package:flipper_services/secure_storage.dart';
 import 'package:googleapis/drive/v3.dart' as ga;
 import 'package:flipper_models/isar_models.dart';
-// import 'package:flutter_firebase_chat_core/flutter_firebase_chat_core.dart';
 import 'package:path/path.dart' as path;
-// import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
-// import 'package:path/path.dart';
-// import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-// import 'package:path';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:url_launcher/url_launcher.dart';
-// import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
-// import 'package:googleapis_auth/auth_browser.dart';
+import 'package:flipper_services/secure_storage.dart';
 
 FileUploaded fileUploadedFromJson(String str) =>
     FileUploaded.fromJson(json.decode(str));
@@ -58,12 +52,13 @@ class FileUploaded {
 // https://developers.google.com/drive/api/v2/appdata
 final _scopes = [
   ga.DriveApi.driveFileScope,
+  ga.DriveApi.driveMetadataScope,
   ga.DriveApi.driveAppdataScope,
-  // ga.DriveApi.driveScope,
-  // ga.DriveApi.driveMetadataScope,
+  ga.DriveApi.driveScope
 ];
 
 class GoogleDrive {
+  final storage = SecureStorage();
   // final log = getLogger('GoogleDrive');
 
   /// if the  var credentials = await storage.getCredentials();
@@ -75,7 +70,21 @@ class GoogleDrive {
   /// because there is known issue https://stackoverflow.com/questions/27890737/firebase-google-auth-offline-access-type-in-order-to-get-a-token-refresh
   /// we silently authenticate and get the token using the silent authentication
   /// for the second time
-  Future<http.Client> obtainAuthenticatedClient() async {}
+  Future<http.Client> obtainAuthenticatedClient() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final idToken = await currentUser!.getIdToken(true);
+    final re = await currentUser.getIdTokenResult(true);
+
+    return authenticatedClient(
+      http.Client(),
+      AccessCredentials(
+        AccessToken("Bearer", idToken!, re.expirationTime!),
+        // refreshToken can be null as it was not returned when logging in
+        currentUser.refreshToken,
+        _scopes,
+      ),
+    );
+  }
 
   prompt(String url) {
     launchUrl(Uri.parse(url));
@@ -111,7 +120,6 @@ class GoogleDrive {
   Future<void> updateBusiness(Business business) async {
     business.backUpEnabled = true;
     business.lastDbBackup = DateTime.now().toIso8601String();
-    // log.i(business.toJson());
 
     /// notify the online that user has enabled the backup
     /// also update the property locally.
