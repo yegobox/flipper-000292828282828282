cde9ed3fb0150ee5f0c430033459f602913dac68
Richard Muragijimana <beastar457@gmail.com>
2023-10-23 13:55:30 +0200

    uat

--- git.sh
+++ git.sh
@@ -10,7 +10,7 @@ cd .. && cd qr_code_scanner &&  git checkout master  && git add . && git commit 
 cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 # cd .. && cd system_time_check &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
 cd .. && cd flutter_list_drag_and_drop &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
-cd .. && cd flutterfire_desktop   &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
+# cd .. && cd flutterfire_desktop   &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 # cd .. && cd plus_plugins &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 # cd .. && cd flutter_slidable &&  git checkout dev  && git add .  && git commit -am "clean-up" && git push origin dev
 # cd .. && cd flutter_datetime_picker &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
