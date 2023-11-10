411beecfb9fcbfc42100ab4f0e7ed576f258c140
Richard Muragijimana <beastar457@gmail.com>
2023-11-10 13:38:28 +0200

    fix order summary
    Former-commit-id: 6d248015da05aca7c75f1ffa16714aa570491dda

--- packages/flipper_dashboard/lib/order_summary_items.dart
+++ packages/flipper_dashboard/lib/order_summary_items.dart
@@ -5,19 +5,17 @@ import 'package:flipper_models/isar_models.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 List<Widget> buildItems({
-  required Function callback,
+  required Function(TransactionItem)
+      callback, // Changed the type of the callback parameter
   required BuildContext context,
   required List<TransactionItem> items,
 }) {
-  final List<Widget> list = [];
-
+  // Check if the list is empty.
   if (items.isEmpty) {
-    list.add(
+    return [
       Column(
         children: [
-          SizedBox(
-            height: 120,
-          ),
+          SizedBox(height: 120),
           Text(
             'Current transaction has no items',
             style: GoogleFonts.poppins(
@@ -28,81 +26,81 @@ List<Widget> buildItems({
           ),
         ],
       ),
-    );
-    return list;
+    ];
   }
 
-  for (TransactionItem item in items) {
-    list.add(
-      Slidable(
-        key: ValueKey(item.id),
-        endActionPane: ActionPane(
-          motion: const ScrollMotion(),
-          children: [
-            SlidableAction(
-              onPressed: (_) => callback(item),
-              backgroundColor: const Color(0xFFFE4A49),
-              foregroundColor: Colors.white,
-              icon: Icons.delete,
-              label: 'Delete',
-            ),
-          ],
+  // Create a list of widgets for each item in the list.
+  return items.map((item) {
+    return Slidable(
+      key: ValueKey(item.id),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) =>
+                callback(item), // Added the context argument
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) =>
+                callback(item), // Added the context argument
+            backgroundColor: const Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.only(left: 40.0, right: 40.0),
+        trailing: Text(
+          'RWF ${NumberFormat('#,###').format(item.price * item.qty)}',
+          style: GoogleFonts.poppins(
+            fontWeight: FontWeight.w400,
+            fontSize: 15,
+            color: Colors.black,
+          ),
         ),
-        startActionPane: ActionPane(
-          motion: const ScrollMotion(),
-          children: [
-            SlidableAction(
-              onPressed: (_) => callback(item),
-              backgroundColor: const Color(0xFFFE4A49),
-              foregroundColor: Colors.white,
-              icon: Icons.delete,
-              label: 'Delete',
-            ),
-          ],
-        ),
-        child: ListTile(
-          contentPadding: const EdgeInsets.only(left: 40.0, right: 40.0),
-          trailing: Text(
-            'RWF ' + NumberFormat('#,###').format(item.price * item.qty),
-            style: GoogleFonts.poppins(
-              fontWeight: FontWeight.w400,
-              fontSize: 15,
-              color: Colors.black,
+        leading: Container(
+          child: Flexible(
+            child: Text(
+              item.name.substring(0, 10),
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w400,
+                fontSize: 15,
+                color: Colors.black,
+              ),
             ),
           ),
-          leading: Container(
-            child: Flexible(
-              child: Text(
-                item.name.substring(0, 10),
-                overflow: TextOverflow.ellipsis,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 15,
+        ),
+        title: Container(
+          child: Flexible(
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.close,
                   color: Colors.black,
+                  size: 16.0,
                 ),
-              ),
-            ),
-          ),
-          title: Container(
-            child: Flexible(
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.close,
-                    color: Colors.black,
-                    size: 16.0,
-                  ),
-                  const Text(' '),
-                  Text(
-                    item.qty.toInt().toString(),
-                  ),
-                ],
-              ),
+                const Text(' '),
+                Text(
+                  item.qty.toInt().toString(),
+                ),
+              ],
             ),
           ),
         ),
+        onTap: () => callback(item), // Added the value argument
       ),
     );
-  }
-  return list;
+  }).toList();
 }
