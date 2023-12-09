95429fdd0c62e320d8fba0fded24f9bdd50ba431
Richard Muragijimana <beastar457@gmail.com>
2023-12-09 10:41:47 +0200

    ...
    Former-commit-id: 24ecbfacd5fe60061140745f09287060c52f864d

--- packages/flipper_models/lib/FirestoreSync.dart
+++ packages/flipper_models/lib/FirestoreSync.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flipper_services/proxy.dart';
 import 'remote_service.dart';
 import 'sync.dart';
-import 'package:flutter/foundation.dart';
 
 abstract class SyncFirestore<M extends IJsonSerializable> implements Sync {
   Future<void> onSave<T extends IJsonSerializable>({required T item});
@@ -16,21 +15,15 @@ class FirestoreSync<M extends IJsonSerializable>
     implements SyncFirestore<M> {
   @override
   Future<void> onSave<T extends IJsonSerializable>({required T item}) async {
+    final String collectionName = getCollectionName<T>();
+    final collectionRef = FirebaseFirestore.instance.collection(collectionName);
     try {
-      await compute(_saveToFirestore<T>, {'item': item});
+      final doc = collectionRef.doc(getItemId<T>(item));
+      await doc.set(item.toJson(), SetOptions(merge: true));
     } catch (e) {
       print('Error: $e');
       // Handle the error appropriately.
     }
-  }
-
-  Future<void> _saveToFirestore<T extends IJsonSerializable>(
-      Map<String, dynamic> data) async {
-    final T item = data['item'];
-    final String collectionName = getCollectionName<T>();
-    final collectionRef = FirebaseFirestore.instance.collection(collectionName);
-    final doc = collectionRef.doc(getItemId<T>(item));
-    await doc.set(item.toJson(), SetOptions(merge: true));
   }
 
   String getCollectionName<T>() {
