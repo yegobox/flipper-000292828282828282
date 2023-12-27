c7cf935285e29245f7b10f283478973648ac0327
Richie@yegobox <info@yegobox.com>
2023-12-27 12:51:00 +0200

    integration testing in progress
    Former-commit-id: 15d5391c607449521cd320b9dabefcc1cab5c4d6

---
+++ packages/flipper_services/lib/mocks/SharedPreferenceStorageMock.dart
@@ -0,0 +1,174 @@
+import 'package:flipper_services/abstractions/storage.dart';
+
+class SharedPreferenceStorageMock implements LocalStorage {
+  @override
+  Future<bool> clear() {
+    // TODO: implement clear
+    throw UnimplementedError();
+  }
+
+  @override
+  int? currentOrderId() {
+    // TODO: implement currentOrderId
+    throw UnimplementedError();
+  }
+
+  @override
+  String? gdID() {
+    // TODO: implement gdID
+    throw UnimplementedError();
+  }
+
+  @override
+  String? getBearerToken() {
+    // TODO: implement getBearerToken
+    throw UnimplementedError();
+  }
+
+  @override
+  int? getBranchId() {
+    // TODO: implement getBranchId
+    throw UnimplementedError();
+  }
+
+  @override
+  int? getBusinessId() {
+    // TODO: implement getBusinessId
+    throw UnimplementedError();
+  }
+
+  @override
+  String getDefaultApp() {
+    // TODO: implement getDefaultApp
+    throw UnimplementedError();
+  }
+
+  @override
+  bool? getIsTokenRegistered() {
+    // TODO: implement getIsTokenRegistered
+    throw UnimplementedError();
+  }
+
+  @override
+  bool getNeedAccountLinkWithPhone() {
+    // TODO: implement getNeedAccountLinkWithPhone
+    throw UnimplementedError();
+  }
+
+  @override
+  String? getServerUrl() {
+    // TODO: implement getServerUrl
+    throw UnimplementedError();
+  }
+
+  @override
+  int? getUserId() {
+    return 24300;
+  }
+
+  @override
+  String? getUserPhone() {
+    // TODO: implement getUserPhone
+    throw UnimplementedError();
+  }
+
+  @override
+  bool hasSignedInForAutoBackup() {
+    // TODO: implement hasSignedInForAutoBackup
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isAnonymous() {
+    // TODO: implement isAnonymous
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isAutoBackupEnabled() {
+    // TODO: implement isAutoBackupEnabled
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isAutoPrintEnabled() {
+    // TODO: implement isAutoPrintEnabled
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isPoroformaMode() {
+    // TODO: implement isPoroformaMode
+    throw UnimplementedError();
+  }
+
+  @override
+  bool isTrainingMode() {
+    // TODO: implement isTrainingMode
+    throw UnimplementedError();
+  }
+
+  @override
+  String? paginationCreatedAt() {
+    // TODO: implement paginationCreatedAt
+    throw UnimplementedError();
+  }
+
+  @override
+  int? paginationId() {
+    // TODO: implement paginationId
+    throw UnimplementedError();
+  }
+
+  @override
+  bool? readBool({required String key}) {
+    // TODO: implement readBool
+    throw UnimplementedError();
+  }
+
+  @override
+  int? readInt({required String key}) {
+    // TODO: implement readInt
+    throw UnimplementedError();
+  }
+
+  @override
+  String? readString({required String key}) {
+    // TODO: implement readString
+    throw UnimplementedError();
+  }
+
+  @override
+  remove({required String key}) {
+    // TODO: implement remove
+    throw UnimplementedError();
+  }
+
+  @override
+  String? whatsAppToken() {
+    // TODO: implement whatsAppToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> writeBool({required String key, required bool value}) {
+    // TODO: implement writeBool
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> writeInt({required String key, required int value}) {
+    // TODO: implement writeInt
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> writeString({required String key, required String value}) {
+    // TODO: implement writeString
+    throw UnimplementedError();
+  }
+
+  Future<LocalStorage> initializePreferences() async {
+    return this;
+  }
+}
--- test/app_test.dart
+++ test/app_test.dart
@@ -1,4 +1,12 @@
-import 'package:flipper_models/MockHttpClient.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
+import 'package:flipper_models/mocks/isarApiMock.dart';
+import 'package:flipper_rw/StateObserver.dart';
+import 'package:flipper_rw/flipper_localize/lib/flipper_localize.dart';
+import 'package:flipper_services/proxy.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flipper_routing/app.locator.dart' as loc;
 import 'package:flipper_routing/all_routes.dart';
@@ -7,27 +15,78 @@ import 'package:flipper_routing/app.dialogs.dart';
 import 'package:flipper_routing/app.router.dart';
 import 'package:flipper_services/locator.dart';
 import 'package:flipper_dashboard/startup_view.dart'; // Import the relevant file
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:nock/nock.dart';
+import 'package:overlay_support/overlay_support.dart';
 
+// https://stackoverflow.com/questions/69248403/flutter-widget-testing-with-httpclient
+//https://pub.dev/packages/nock
+//https://github.com/nock/nock?tab=readme-ov-file#how-does-it-work
+//https://designer.mocky.io/
 void main() {
   // Mock the HTTP client at the ViewModel level
-  MockHttpClient mockHttpClient = MockHttpClient();
+  setUpAll(nock.init);
 
-  // Move the setup logic outside of the test
   setUp(() async {
+    nock.cleanAll();
     await initDependencies();
     loc.setupLocator(
       stackedRouter: stackedRouter,
     );
     setupDialogUi();
     setupBottomSheetUi();
-
-    // Provide the mock HTTP client to the ViewModel
-    loc.locator.registerLazySingleton(() => mockHttpClient);
+    loc.locator.registerLazySingleton(() => IsarAPIMock());
   });
 
   testWidgets('App start and runs', (WidgetTester tester) async {
+    // int? userId = ProxyService.box.getUserId();
+    // final interceptor =
+    //     nock("https://apihub.yegobox.com/v2/api").get("/businessUserId/$userId")
+    //       ..reply(
+    //         200,
+    //         "result",
+    //       );
+
+    // expect(interceptor.isDone, true);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const StartUpView());
+    await tester.pumpWidget(ProviderScope(
+      observers: [StateObserver()],
+      child: OverlaySupport.global(
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: true,
+          title: 'flipper',
+          // Define the light theme for the app, based on defined colors and
+          // properties above.
+          //TODOimplement my own as this is killing design
+          // theme: GThemeGenerator.generate(),
+          // darkTheme: GThemeGenerator.generateDark(),
+          theme: ThemeData(
+            textTheme: GoogleFonts.poppinsTextTheme(),
+          ),
+          localizationsDelegates: [
+            FirebaseUILocalizations.withDefaultOverrides(
+              const LabelOverrides(),
+            ),
+            const FlipperLocalizationsDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            CountryLocalizations.delegate
+          ],
+          supportedLocales: const [
+            Locale('en'), // English
+            Locale('es'), // Spanish
+          ],
+          locale: const Locale('en'),
+          // locale: model
+          //     .languageService.locale,
+          // themeMode: model.settingService.themeMode.value,
+          themeMode: ThemeMode.system,
+          routerDelegate: stackedRouter.delegate(),
+          routeInformationParser: stackedRouter.defaultRouteParser(),
+        ),
+      ),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('A revolutionary business software ...'), findsOneWidget);
