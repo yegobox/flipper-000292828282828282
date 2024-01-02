fd20ef9e01158b284fe62ed7c1976a789800fe30
Richie@yegobox <info@yegobox.com>
2024-01-02 19:55:42 +0200

    ...

--- packages/flipper_services/lib/constants.dart
+++ packages/flipper_services/lib/constants.dart
@@ -204,3 +204,11 @@ abstract class AppIcons {
   static const String windowsSymbolicWithNotificationBadge =
       '$path/$kPackageId-symbolic-with-notification-badge.ico';
 }
+
+ Color getColorFromHex(String hexColor) {
+    hexColor = hexColor.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return Color(int.parse(hexColor, radix: 16));
+  }
