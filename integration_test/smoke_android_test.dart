0e54ce8bcfefdce52e2ca2c58ba3083f9d8671c3
Richie@yegobox <info@yegobox.com>
2024-01-15 09:05:12 +0200

    another attempt
    Former-commit-id: c6e47b313b2daedaa4d64f4d979b27a4018c4bd0

--- integration_test/smoke_android_test.dart
+++ integration_test/smoke_android_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nock/nock.dart';
 import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test.dart';
-
+import 'package:flutter/material.dart';
 import 'package:flipper_rw/main.dart' as app;
 
 // https://stackoverflow.com/questions/69248403/flutter-widget-testing-with-httpclient
@@ -13,7 +13,7 @@ import 'package:flipper_rw/main.dart' as app;
 //flutter test --dart-define=Test=true -d R58MC1HCNFT integration_test/smoke_android_test.dart
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  binding.defaultTestTimeout = Timeout.none;
+  WidgetsFlutterBinding.ensureInitialized();
 
   setUpAll(nock.init);
 
