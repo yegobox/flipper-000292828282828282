2686424247891b11fca0764eff06d6363cf40bae
Richie@yegobox <info@yegobox.com>
2024-01-26 23:22:19 +0200

    switch to api key
    Former-commit-id: 75241602c53190b36cac408c9f7de6d6b7955ea9

--- packages/flipper_models/lib/RealmSync.dart
+++ packages/flipper_models/lib/RealmSync.dart
@@ -77,12 +77,12 @@ class RealmSync<M extends IJsonSerializable>
           ///You can use this callback to notify the user before the reset begins.
         },
       ),
-      // shouldCompactCallback: ((totalSize, usedSize) {
-      //   // Compact if the file is over 10MB in size and less than 50% 'used'
-      //   const tenMB = 10 * 1048576;
-      //   return (totalSize > tenMB) &&
-      //       (usedSize.toDouble() / totalSize.toDouble()) < 0.5;
-      // }),
+      shouldCompactCallback: ((totalSize, usedSize) {
+        // Compact if the file is over 10MB in size and less than 50% 'used'
+        const tenMB = 10 * 1048576;
+        return (totalSize > tenMB) &&
+            (usedSize.toDouble() / totalSize.toDouble()) < 0.5;
+      }),
     );
     // realm = await Realm.open(config);
     CancellationToken token = CancellationToken();
--- packages/flipper_models/lib/isolateHandelr.dart
+++ packages/flipper_models/lib/isolateHandelr.dart
@@ -59,6 +59,12 @@ mixin IsolateHandler {
         RealmIUnit.schema
       ],
       path: await absolutePath("db_"),
+      shouldCompactCallback: ((totalSize, usedSize) {
+        // Compact if the file is over 10MB in size and less than 50% 'used'
+        const tenMB = 10 * 1048576;
+        return (totalSize > tenMB) &&
+            (usedSize.toDouble() / totalSize.toDouble()) < 0.5;
+      }),
     );
 
     final realm = Realm(config);
