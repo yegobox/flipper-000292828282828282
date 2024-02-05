153383ca82984fb28f76f308cea5bf67ab6b82d8
Richard M <murag.richard@gmail.com>
2024-02-05 10:19:07 +0200

    on desktop should not use anonymous user to login

--- packages/flipper_models/lib/view_models/login_viewmodel.dart
+++ packages/flipper_models/lib/view_models/login_viewmodel.dart
@@ -32,6 +32,13 @@ class LoginViewModel extends FlipperBaseModel {
     ProxyService.box.writeString(key: 'otp', value: ot);
   }
 
+  bool isEmail(String input) {
+    // Implement your logic to check if input is an email
+    // You can use regular expressions or any other email validation mechanism
+    // For simplicity, this example checks if the input contains '@'
+    return input.contains('@');
+  }
+
   bool _isProceeding = false;
 
   get isProcessing => _isProceeding;
@@ -58,8 +65,62 @@ class LoginViewModel extends FlipperBaseModel {
 
       // Clear and re-sign in with Firebase anonymously
       await FirebaseAuth.instance.signOut();
-      await FirebaseAuth.instance.signInAnonymously();
 
+      /// we are no longer signin anonymously
+      ///await FirebaseAuth.instance.signInAnonymously();
+
+      String phoneNumber = pin.phoneNumber;
+      if (!isEmail(phoneNumber)) {
+        // Append '@flipper.rw' to make it an email
+        phoneNumber = phoneNumber + '@flipper.rw';
+      }
+      try {
+        // Try to sign in with the provided credentials
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: phoneNumber,
+          password: phoneNumber,
+        );
+      } on FirebaseAuthException catch (e) {
+        // Handle authentication exceptions
+        if (e.code == 'user-not-found') {
+          try {
+            // User not found, attempt to create a new account
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+              email: phoneNumber,
+              password: phoneNumber,
+            );
+          } on FirebaseAuthException catch (e) {
+            // Handle exceptions during account creation
+            if (e.code == 'weak-password') {
+              // Password is too weak
+              print('Error: The password provided is too weak.');
+            } else if (e.code == 'email-already-in-use') {
+              // Account already exists for the provided email
+              print('Error: The account already exists for that email.');
+            } else {
+              // Handle other exceptions during account creation
+              print('Error: $e');
+            }
+          } catch (e) {
+            // Handle unexpected errors during account creation
+            print('Error: $e');
+          }
+        } else if (e.code == 'wrong-password') {
+          // Wrong password provided for the user
+          print('Error: Wrong password provided for that user.');
+        } else {
+          // Handle other authentication exceptions
+          print('Error: $e');
+        }
+      } catch (e) {
+        // Handle unexpected errors during sign-in
+        print('Error: $e');
+      }
+
+      /// now ling this user with credentials so next time he wont be anonymous
+      //NOTE: if (FirebaseAuth.instance.currentUser!.providerData.isEmpty) {
+      //   // User is not linked with any providers
+      // }
       // Check if a logged-in Firebase user exists
       final auth = FirebaseAuth.instance;
       if (auth.currentUser != null) {
