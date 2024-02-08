165d6086286ce93c9210a8ea03d8d3d37db22030
Richard M <murag.richard@gmail.com>
2024-02-08 11:48:59 +0200

    avoid touching on lastTouched when receiving data on other device, this lead to incorrect data on dashboard

--- packages/flipper_dashboard/lib/apps.dart
+++ packages/flipper_dashboard/lib/apps.dart
@@ -284,10 +284,8 @@ class _AppsState extends ConsumerState<Apps> {
       List<ITransaction> transactions, String transactionPeriod) {
     DateTime oldDate = _calculateStartingDate(transactionPeriod);
     List<ITransaction> filteredTransactions = transactions
-        .where((transaction) => DateTime.parse(
-                transaction.lastTouched?.toIso8601String() ??
-                    transaction.createdAt)
-            .isAfter(oldDate))
+        .where((transaction) =>
+            DateTime.parse(transaction.createdAt).isAfter(oldDate))
         .toList();
     double sumCashIn = 0;
     for (final transaction in filteredTransactions) {
@@ -318,10 +316,8 @@ class _AppsState extends ConsumerState<Apps> {
       List<ITransaction> transactions, String transactionPeriod) {
     DateTime oldDate = _calculateStartingDate(transactionPeriod);
     List<ITransaction> filteredTransactions = transactions
-        .where((transaction) => DateTime.parse(
-                transaction.lastTouched?.toIso8601String() ??
-                    transaction.createdAt)
-            .isAfter(oldDate))
+        .where((transaction) =>
+            DateTime.parse(transaction.createdAt).isAfter(oldDate))
         .toList();
     double sumCashOut = 0;
     for (final transaction in filteredTransactions) {
@@ -337,10 +333,8 @@ class _AppsState extends ConsumerState<Apps> {
     log(transactions.length.toString(), name: 'render transactions on gauge');
     DateTime startingDate = _calculateStartingDate(transactionPeriod);
     return transactions
-        .where((transaction) => DateTime.parse(
-                transaction.lastTouched?.toIso8601String() ??
-                    transaction.createdAt)
-            .isAfter(startingDate))
+        .where((transaction) =>
+            DateTime.parse(transaction.createdAt).isAfter(startingDate))
         .toList();
   }
 
@@ -410,9 +404,7 @@ class _AppsState extends ConsumerState<Apps> {
 
           List<ITransaction> filteredTransactions = [];
           for (final transaction in transactions) {
-            temporaryDate = DateTime.parse(
-                transaction.lastTouched?.toIso8601String() ??
-                    transaction.createdAt);
+            temporaryDate = DateTime.parse(transaction.createdAt);
             if (temporaryDate.isAfter(oldDate)) {
               filteredTransactions.add(transaction);
             }
