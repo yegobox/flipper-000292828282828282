dbe69b0470db17e4a130150f9cd031134776f814
Richard M <murag.richard@gmail.com>
2024-02-17 22:16:39 +0200

    update the latest flutter 3.19 and everything that comes with it

--- git.sh
+++ git.sh
@@ -7,7 +7,7 @@ cd .. && cd receipt &&  git checkout main  && git add .   && git commit -am "cle
 cd .. && cd kds &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 cd .. && cd qr_code_scanner &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 # cd .. && cd pubnub &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
-cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
+# cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 # cd .. && cd system_time_check &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
 cd .. && cd flutter_list_drag_and_drop &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 
