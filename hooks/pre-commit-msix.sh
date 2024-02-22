6ed540f5280512f00b82b558d5a091e07f98c0b3
Richie <beastar457@gmail.com>
2024-02-22 19:32:14 +0200

    pre-commit
    Former-commit-id: 6a74552b8102a0e4efa454cfe21001de2dccb81f

--- hooks/pre-commit-msix.sh
+++ hooks/pre-commit-msix.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Get current msix_version
-msix_version=$(awk '/msix_version:/ {print $2}' pubspec.yaml)
+## first go to then to apps/main directory
+cd apps/main_app
+msix_version=$(awk '/msix_version:/ {print $2}' apps/main_app/pubspec.yaml)
 
 # Increment the third number
 IFS='.' read -ra version_parts <<< "$msix_version"
@@ -10,7 +12,7 @@ version_parts[3]=0  # Set the last number to zero
 new_msix_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}.${version_parts[3]}"
 
 # Replace msix_version in pubspec.yaml
-sed -i "s/msix_version: $msix_version/msix_version: $new_msix_version/" pubspec.yaml
+sed -i "s/msix_version: $msix_version/msix_version: $new_msix_version/" apps/main_app/pubspec.yaml
 
 # Commit
 git add pubspec.yaml
