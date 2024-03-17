2b8967a698b003eb6c4389842219b9bb635d156e
Muragijimana <info@yegobox.com>
2024-03-17 20:43:44 +0200

    fixing scripts
    Former-commit-id: 0fdfa4ba0607387622a2e0463edb9200465be399

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -20,58 +20,52 @@ git add pubspec.yaml
 #git commit -m "Bump version to $new_version"
 
 
-# Get current msix_version
-- name: Increment MSIX version
-  shell: bash
-  run: |
-    # Get current msix_version
-    msix_version=$(awk '/msix_version:/ {print $2}' pubspec.yaml)
+# Get current operating system
 
-    # Increment the third number
-    IFS='.' read -ra version_parts <<< "$msix_version"
-    version_parts[2]=$((version_parts[2]+1))
-    version_parts[3]=0
+OS=$(uname)
 
-    # Set the new msix_version
-    new_msix_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}.${version_parts[3]}"
+if [ "$OS" == "Darwin" ] || [ "$OS" == "Linux" ]; then
+  # Get current msix_version
+  msix_version=$(awk '/msix_version:/ {print $2}' pubspec.yaml)
 
-    # Replace msix_version in pubspec.yaml
-    if [ "$RUNNER_OS" == "macOS" ]; then
-      sed -i.bak "s/msix_version: $msix_version/msix_version: $new_msix_version/" pubspec.yaml
-      rm pubspec.yaml.bak
-    elif [ "$RUNNER_OS" == "Linux" ]; then
-      sed -i "s/msix_version: $msix_version/msix_version: $new_msix_version/" pubspec.yaml
-    else
-      echo "::warning::Unsupported operating system: $RUNNER_OS. Skipping version increment."
-      exit 0
-    fi
+  # Increment the third number
+  IFS='.' read -ra version_parts <<< "$msix_version"
+  version_parts[2]=$((version_parts[2]+1))
+  version_parts[3]=0
 
-    # Commit
+  # Set the new msix_version
+  new_msix_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}.${version_parts[3]}"
+
+  # Replace msix_version in pubspec.yaml
+  sed -i.bak "s/msix_version: $msix_version/msix_version: $new_msix_version/" pubspec.yaml && rm pubspec.yaml.bak
+
+  # Commit if the version has changed
+  if [ "$msix_version" != "$new_msix_version" ]; then
     git add pubspec.yaml
     git commit -m "Increment MSIX version to $new_msix_version"
+  fi
+elif [ "$OS" == "Windows" ]; then
+  # Get current msix_version
+  msix_version=$(awk '/msix_version:/ {print $2}' pubspec.yaml)
 
-- name: Increment MSIX version (Windows)
-  shell: pwsh
-  run: |
-    # Get current msix_version
-    $msix_version = (Get-Content pubspec.yaml -Raw) -match 'msix_version:\s*(.+?)(?=\s)' | ForEach-Object { $_.Captures.Groups[1].Value }
+  # Increment the third number
+  IFS='.' read -ra version_parts <<< "$msix_version"
+  version_parts[2]=$((version_parts[2]+1))
+  version_parts[3]=0
 
-    # Increment the third number
-    $version_parts = $msix_version.Split('.')
-    $version_parts[2] = [int]$version_parts[2] + 1
-    $version_parts[3] = 0
+  # Set the new msix_version
+  new_msix_version="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}.${version_parts[3]}"
 
-    # Set the new msix_version
-    $new_msix_version = $version_parts -join '.'
+  # Replace msix_version in pubspec.yaml
+  sed -i "s/msix_version: $msix_version/msix_version: $new_msix_version/" pubspec.yaml
 
-    # Replace msix_version in pubspec.yaml
-    $pubspec_content = Get-Content pubspec.yaml -Raw
-    $pubspec_content = $pubspec_content -replace "msix_version:\s*$msix_version", "msix_version: $new_msix_version"
-    Set-Content pubspec.yaml $pubspec_content
-
-    # Commit
+  # Commit if the version has changed
+  if [ "$msix_version" != "$new_msix_version" ]; then
     git add pubspec.yaml
     git commit -m "Increment MSIX version to $new_msix_version"
+  fi
+else
+  echo "::warning::Unsupported operating system: $OS. Skipping version increment."
+fi
 
-  # Run this step only on Windows
-  if: runner.os == 'Windows'
+
