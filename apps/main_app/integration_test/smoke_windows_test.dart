e5bc27e397283317aa74ae9b24c6967eb1822e80
Muragijimana <info@yegobox.com>
2024-03-18 11:12:33 +0200

    add more test
    Former-commit-id: 579698c1e6e519d2d63feee55336282534ba135f

--- apps/main_app/integration_test/smoke_windows_test.dart
+++ apps/main_app/integration_test/smoke_windows_test.dart
@@ -7,6 +7,8 @@ import 'package:integration_test/integration_test.dart';
 
 import 'package:flipper_rw/main.dart' as app;
 
+import 'test_cases.dart';
+
 // https://stackoverflow.com/questions/69248403/flutter-widget-testing-with-httpclient
 //https://pub.dev/packages/nock
 //https://github.com/nock/nock?tab=readme-ov-file#how-does-it-work
@@ -15,51 +17,21 @@ import 'package:flipper_rw/main.dart' as app;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
-  // Mock the HTTP client at the ViewModel level
   setUpAll(nock.init);
 
   group('Complete E2E Test:', () {
     testWidgets('App start and runs windows', (WidgetTester tester) async {
-      await tester.runAsync(() async {
-        await app.main();
-        await tester.pumpAndSettle(const Duration(seconds: 12));
-
-        expect(find.text('Log in to Flipper by QR Code'), findsOneWidget);
-
-        await tester.tap(find.byKey(const Key('pinLogin')));
-        await tester.pumpAndSettle(const Duration(seconds: 2));
-
-        // Verify that the PIN text field is rendered within the Form
-        expect(find.byType(Form), findsOneWidget);
-
-        expect(find.byType(TextFormField), findsOneWidget);
-
-        // Simulate entering an empty PIN
-        await tester.enterText(find.byType(TextFormField), '');
-
-        // Verify that the validator error message is displayed
-        await tester.tap(find.text('Log in'));
-
-        await tester.pumpAndSettle(const Duration(seconds: 2));
-
-        expect(find.text('PIN is required'), findsOneWidget);
-
-        // Simulate entering a non-empty PIN
-        await tester.enterText(find.byType(TextFormField), '1234');
-        await tester.tap(find.text('Log in'));
-
-
-        /// now test with real PIN. it login and go to openDrawerPage
-        await tester.enterText(find.byType(TextFormField), '67814');
-
-        await tester.tap(find.text('Log in'));
-
-        await tester.pumpAndSettle(const Duration(seconds: 10));
-
-        await tester.tap(find.byKey(const Key('openDrawerPage')));
-
-      });
+      await tester.runAsync(testAppStartAndRunsWindows);
     });
   });
 }
+
+Future<void> testAppStartAndRunsWindows(WidgetTester tester) async {
+  await startAppAndPump(tester);
+  await verifyLoginPage(tester);
+  await enterEmptyPINAndVerifyErrorMessage(tester);
+  // await enterNonEmptyPINAndLogIn(tester);
+}
+
+
 
---
+++ apps/main_app/integration_test/test_cases.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+// Import the relevant file
+import 'package:nock/nock.dart';
+
+import 'package:integration_test/integration_test.dart';
+
+import 'package:flipper_rw/main.dart' as app;
+
+/// desktop test cases
+Future<void> startAppAndPump(WidgetTester tester) async {
+  await app.main();
+  await tester.pumpAndSettle(const Duration(seconds: 12));
+}
+
+Future<void> verifyLoginPage(WidgetTester tester) async {
+  expect(find.text('Log in to Flipper by QR Code'), findsOneWidget);
+  await tester.tap(find.byKey(const Key('pinLogin')));
+  await tester.pumpAndSettle(const Duration(seconds: 2));
+  expect(find.byType(Form), findsOneWidget);
+  expect(find.byType(TextFormField), findsOneWidget);
+}
+
+Future<void> enterEmptyPINAndVerifyErrorMessage(WidgetTester tester) async {
+  await tester.enterText(find.byType(TextFormField), '');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 2));
+  expect(find.text('PIN is required'), findsOneWidget);
+}
+
+Future<void> enterNonEmptyPINAndLogIn(WidgetTester tester) async {
+  await tester.enterText(find.byType(TextFormField), '1234');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 10));
+  await tester.enterText(find.byType(TextFormField), '67814');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 10));
+  await tester.tap(find.byKey(const Key('openDrawerPage')));
+}
+/// end of desktop
+
+/// start of android
+
+Future<void> runAppAndVerifyInitialScreen(WidgetTester tester) async {
+  await app.main();
+  await tester.pumpAndSettle();
+  expect(find.text('Create Account'), findsOneWidget);
+  expect(find.text('Sign In'), findsOneWidget);
+}
+
+Future<void> tapSignInButtonAndVerifyOptions(WidgetTester tester) async {
+  await tester.tap(find.byKey(const Key('signInButtonKey')));
+  await tester.pumpAndSettle();
+  expect(find.text("Phone Number"), findsOneWidget);
+  expect(find.byKey(const Key('phoneNumberLogin')), findsOneWidget);
+  expect(find.byKey(const Key('googleLogin')), findsOneWidget);
+  expect(find.byKey(const Key('microsoftLogin')), findsOneWidget);
+  expect(find.text("How would you like to proceed?"), findsOneWidget);
+}
+
+Future<void> testPINLogin(WidgetTester tester) async {
+  await tester.tap(find.byKey(const Key('pinLogin')));
+  await tester.pumpAndSettle();
+  expect(find.byType(Form), findsOneWidget);
+  expect(find.byType(TextFormField), findsOneWidget);
+
+  // Simulate entering an empty PIN
+  await tester.enterText(find.byType(TextFormField), '');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 2));
+  expect(find.text('PIN is required'), findsOneWidget);
+
+  // Simulate entering a non-empty PIN
+  await tester.enterText(find.byType(TextFormField), '1234');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 5));
+
+  // Log in with real PIN and go to openDrawerPage
+  await tester.enterText(find.byType(TextFormField), '67814');
+  await tester.tap(find.text('Log in'));
+  await tester.pumpAndSettle(const Duration(seconds: 10));
+  await tester.tap(find.byKey(const Key('openDrawerPage')));
+}
