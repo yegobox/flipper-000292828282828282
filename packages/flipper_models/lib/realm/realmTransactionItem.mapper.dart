3f8b0d8affb77fa546589407c3baedd2d451465d
Muragijimana <info@yegobox.com>
2024-04-25 23:47:28 +0200

    commit
    Former-commit-id: aa77fbb3aff0a8e2903e49b5da8cfcffa6aece14

--- packages/flipper_models/lib/realm/realmTransactionItem.mapper.dart
+++ packages/flipper_models/lib/realm/realmTransactionItem.mapper.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, unnecessary_cast
+// ignore_for_file: unused_element, unnecessary_cast, override_on_non_overriding_member
 // ignore_for_file: strict_raw_type, inference_failure_on_untyped_parameter
 
 part of 'realmTransactionItem.dart';
@@ -282,11 +282,9 @@ mixin $RealmITransactionItemMappable {
 
   @override
   bool operator ==(Object other) {
-    return identical(this, other) ||
-        (runtimeType == other.runtimeType &&
-            $RealmITransactionItemMapper
-                .ensureInitialized()
-                .isValueEqual(this as $RealmITransactionItem, other));
+    return $RealmITransactionItemMapper
+        .ensureInitialized()
+        .equalsValue(this as $RealmITransactionItem, other);
   }
 
   @override
