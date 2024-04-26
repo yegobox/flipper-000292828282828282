29b8798395221c0fa6760adb4341247a87031457
Muragijimana <info@yegobox.com>
2024-04-26 19:49:16 +0200

    ..

--- packages/flipper_dashboard/lib/transactionList.dart
+++ packages/flipper_dashboard/lib/transactionList.dart
@@ -1,4 +1,4 @@
-import 'package:flipper_dashboard/view_data.dart';
+import 'package:flipper_dashboard/PaginatedDataView.dart';
 import 'package:flipper_models/view_models/mixins/riverpod_states.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -15,7 +15,7 @@ class TransactionList extends ConsumerWidget {
       data: (value) => Container(
         width: 150,
         height: 800,
-        child: ViewData(
+        child: PaginatedDataView(
           transactions: value,
         ),
       ),
