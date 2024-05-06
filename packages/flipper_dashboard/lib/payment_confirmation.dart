9d3ba9e2ec80e0c8e0f6a221ce8945181eeb1eda
Muragijimana <info@yegobox.com>
2024-05-06 13:40:49 +0200

    fix the async issue

--- packages/flipper_dashboard/lib/payment_confirmation.dart
+++ packages/flipper_dashboard/lib/payment_confirmation.dart
@@ -67,11 +67,6 @@ class PaymentConfirmationState extends ConsumerState<PaymentConfirmation> {
       },
       onViewModelReady: (model) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
-          /// reload the  pending transaction
-          ref.refresh(pendingTransactionProvider(TransactionType.cashOut));
-          ref.refresh(pendingTransactionProvider(TransactionType.custom));
-
-          /// end refreshing.
           if (widget.transaction.customerId != null) {
             showDialog(
               context: context,
