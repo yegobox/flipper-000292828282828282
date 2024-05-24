31cf5a84769121be9c848fa46d33061107b795f8
Muragijimana <info@yegobox.com>
2024-05-24 13:16:44 +0200

    update micro

--- git.sh
+++ git.sh
@@ -9,14 +9,14 @@ cd .. && cd qr_code_scanner &&  git checkout master  && git add . && git commit 
 # cd .. && cd pubnub &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 # cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 # cd .. && cd system_time_check &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
-cd .. && cd flutter_list_drag_and_drop &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
+cd .. && cd flutter_list_drag_and_drop && git stash &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 
 # cd .. && cd plus_plugins &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 # cd .. && cd flutter_slidable &&  git checkout dev  && git add .  && git commit -am "clean-up" && git push origin dev
 cd .. && cd flutter_datetime_picker &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flutter_luban && git stash   &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flipper-turbo  &&  git checkout uat  && git add .  && git commit -am "clean-up" && git push origin uat
-# cd .. && cd dart_pdf  &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
+cd .. && cd dart_pdf  &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 
 # These dependencies where local and now depend on them from pubspec
 # I was doing it becase it was using permissions that were not accepted but now I believe they are removed.
