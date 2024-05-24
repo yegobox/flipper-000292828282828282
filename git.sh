7461cf0786cbb58817d2ce0482aa08eb405968aa
Muragijimana <info@yegobox.com>
2024-05-24 13:15:09 +0200

    update micro

--- git.sh
+++ git.sh
@@ -14,7 +14,7 @@ cd .. && cd flutter_list_drag_and_drop &&  git checkout main  && git add .  && g
 # cd .. && cd plus_plugins &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 # cd .. && cd flutter_slidable &&  git checkout dev  && git add .  && git commit -am "clean-up" && git push origin dev
 cd .. && cd flutter_datetime_picker &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
-cd .. && cd flutter_luban  &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
+cd .. && cd flutter_luban && git stash   &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flipper-turbo  &&  git checkout uat  && git add .  && git commit -am "clean-up" && git push origin uat
 # cd .. && cd dart_pdf  &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 
