3075f784f100e5e16a1cfea452f102dce63b8467
Muragijimana <info@yegobox.com>
2024-05-25 15:30:40 +0200

    pull command updated
    Former-commit-id: 559ddff848968942077d73d9fecefd3f20504c42

--- git.sh
+++ git.sh
@@ -6,20 +6,13 @@ cd .. && cd qr.flutter &&  git checkout main  && git add . && git commit -am "cl
 cd .. && cd receipt &&  git checkout main  && git add .   && git commit -am "clean-up" && git push origin main
 cd .. && cd kds &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
 cd .. && cd qr_code_scanner &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
-# cd .. && cd pubnub &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
-# cd .. && cd upgrader &&  git checkout master  && git add . && git commit -am "clean-up" && git push origin master
-# cd .. && cd system_time_check &&  git checkout main  && git add . && git commit -am "clean-up" && git push origin main
+
 cd .. && cd flutter_list_drag_and_drop && git stash &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
 
-# cd .. && cd plus_plugins &&  git checkout main  && git add .  && git commit -am "clean-up" && git push origin main
+
 cd .. && cd flutter_slidable && git stash &&  git checkout dev  && git add .  && git commit -am "clean-up" && git push origin dev
 cd .. && cd flutter_datetime_picker &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flutter_luban && git stash   &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flipper-turbo  &&  git checkout uat  && git add .  && git commit -am "clean-up" && git push origin uat
 cd .. && cd dart_pdf  &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd form_bloc  && git stash &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
-
-# These dependencies where local and now depend on them from pubspec
-# I was doing it becase it was using permissions that were not accepted but now I believe they are removed.
-#dart_pdf
-# flutter_file_picker
--- pull.sh
+++ pull.sh
@@ -6,11 +6,12 @@ cd .. && cd qr.flutter && git checkout main && git pull origin main
 cd .. && cd receipt && git checkout main && git pull origin main
 cd .. && cd kds && git checkout master && git pull origin master
 cd .. && cd qr_code_scanner && git checkout master && git pull origin master
-cd .. && cd upgrader && git checkout master && git pull origin master
-cd .. && cd system_time_check && git checkout main && git pull origin main
-cd .. && cd flutter_list_drag_and_drop && git checkout main && git pull origin main
-cd .. && cd flutterfire_desktop && git checkout main && git pull origin main
-cd .. && cd flutter_slidable && git checkout dev && git pull origin dev
-# cd .. && cd flutter_launcher_icons && git checkout master && git pull origin master
+
+cd .. && cd flutter_list_drag_and_drop && git stash && git checkout main && git pull origin main
+
+cd .. && cd flutter_slidable && git stash && git checkout dev && git pull origin dev
 cd .. && cd flutter_datetime_picker && git checkout master && git pull origin master
-cd .. && cd flutter_luban && git checkout master && git pull origin master
+cd .. && cd flutter_luban && git stash && git checkout master && git pull origin master
+cd .. && cd flipper-turbo && git checkout uat && git pull origin uat
+cd .. && cd dart_pdf && git checkout master && git pull origin master
+cd .. && cd form_bloc && git stash && git checkout master && git pull origin master
