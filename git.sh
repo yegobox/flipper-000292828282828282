3e0a10021ea65030d2dae2bd9ebbfe7b7da2f64e
Muragijimana <info@yegobox.com>
2024-07-20 20:43:21 +0200

    build windows update direct-build-windows

--- git.sh
+++ git.sh
@@ -14,7 +14,7 @@ cd .. && cd flutter_list_drag_and_drop && git stash &&  git checkout main  && gi
 cd .. && cd flutter_slidable && git stash &&  git checkout dev  && git add .  && git commit -am "clean-up" && git push origin dev
 cd .. && cd flutter_datetime_picker &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
 cd .. && cd flutter_luban && git stash   &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
-cd .. && cd flipper-turbo  &&  git checkout uat  && git add .  && git commit -am "clean-up" && git push origin uat
+# cd .. && cd flipper-turbo  &&  git checkout uat  && git add .  && git commit -am "clean-up" && git push origin uat
 cd .. && cd dart_pdf  &&  git checkout fix  && git add .  && git commit -am "clean-up" && git push origin fix
 
 cd .. && cd form_bloc    &&  git checkout master  && git add .  && git commit -am "clean-up" && git push origin master
