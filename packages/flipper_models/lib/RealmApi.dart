6da60098425696f21086f1e03079f835071c3056
Muragijimana <info@yegobox.com>
2024-08-10 16:02:00 +0200

    test direct-build-windows

--- packages/flipper_models/lib/RealmApi.dart
+++ packages/flipper_models/lib/RealmApi.dart
@@ -2972,21 +2972,26 @@ class RealmAPI<M extends IJsonSerializable>
       }).catchError((error) async {
         String path = 'public/${subPath}-$branchId/$assetName';
         try {
-          /// first delete asset from realm
+          /// first check if the asset exists in realm
           Assets? asset = getAsset(assetName: assetName);
-          if (asset != null) {
+          if (asset != null && !asset.isValid) {
+            talker.warning(
+                'Asset object is no longer valid. Skipping deletion from Realm.');
+          } else if (asset != null) {
             ProxyService.realm.realm!.write(() {
               ProxyService.realm.realm!.delete(asset);
             });
+            talker.info('Asset deleted from Realm: $assetName');
           }
+
+          // Attempt to remove the asset from S3 storage
           await amplify.Amplify.Storage.remove(
             path: storagePath,
-            // options: const amplify.StorageRemoveOptions()
           );
           talker.info('Asset deleted from S3 after download error: $path');
         } catch (deleteError) {
           talker.error(
-              'Failed to delete asset from S3: $path - ${deleteError.toString()}');
+              'Failed to delete asset: $path - ${deleteError.toString()}');
         }
         progressController.addError(error);
         progressController.close();
