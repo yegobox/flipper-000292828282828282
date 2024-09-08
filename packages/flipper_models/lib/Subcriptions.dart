5a0c90fa7a18d177141d0c94bec1acbde2ad3284
Muragijimana <info@yegobox.com>
2024-09-08 16:13:20 +0200

    test direct-build-windows fixes #338

--- packages/flipper_models/lib/Subcriptions.dart
+++ packages/flipper_models/lib/Subcriptions.dart
@@ -81,7 +81,7 @@ mixin Subscriptions {
 
       ///TODO: in next release will query location that are in same range with the user longitude and latitude
       'location': realm.all<Location>(),
-      // 'my-stock': realm.query<Stock>(r'branchId == $0', [branchId]),
+      'my-stock': realm.query<Stock>(r'branchId == $0', [branchId]),
     };
 
     // Update subscriptions
