44598d8b5a63f35884b9db47eef7cc0629e2a926
Muragijimana <info@yegobox.com>
2024-09-10 11:14:31 +0200

    addressing #339 test direct-build-windows

--- apps/flipper/lib/dependencyInitializer.dart
+++ apps/flipper/lib/dependencyInitializer.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -81,6 +82,9 @@ Future<void> initializeDependencies() async {
   });
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
   );
   _configureAmplify();
   if (!isWindows) {
