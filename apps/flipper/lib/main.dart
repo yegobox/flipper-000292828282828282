f8b6720016e7c96b7b23652fffaf873c19f3c1bc
Afuwa <lyhamissi@gmail.com>
2024-12-08 20:57:02 +0200

    test direct-build-windows test direct-build-android #4

--- apps/flipper/lib/main.dart
+++ apps/flipper/lib/main.dart
@@ -17,7 +17,7 @@ import 'package:sizer/sizer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/foundation.dart';
 
-//1.1.7
+//1.1.8
 Future<void> main() async {
   await initializeDependencies();
   await SentryFlutter.init(
