67e5fc654fcabcb6bfdd2fb936da71394ff89e50
Muragijimana <info@yegobox.com>
2024-12-18 04:34:55 +0200

    ...

--- apps/flipper/lib/main.dart
+++ apps/flipper/lib/main.dart
@@ -16,7 +16,7 @@ import 'package:sizer/sizer.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/foundation.dart';
 
-//1.1.12
+//1.1.13
 Future<void> main() async {
   await initializeDependencies();
   await SentryFlutter.init(
