8a5b2e16b1670a79b0a820bbff80240b5083365b
Muragijimana <info@yegobox.com>
2025-01-10 12:19:45 +0200

    test direct-build-windows test direct-build-android

--- apps/flipper/lib/main.dart
+++ apps/flipper/lib/main.dart
@@ -17,7 +17,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/foundation.dart';
 
 // net info: billers
-//1.1.13
+//1.1.1
 Future<void> main() async {
   await initializeDependencies();
   await SentryFlutter.init(
